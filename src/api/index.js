898b06cd07a4ee6b7d8e7f8e5fb34659e2dceebd
huzefakm <huzzwork@gmail.com>
2020-10-13 17:51:00 +0530

    refactor: added API endpoints

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,18 @@
 const isBuildVersion = process.env.NODE_ENV === "production";
 
 const BASE_URL = isBuildVersion
-	? "https://dev-finbazaar-admin.tk"
-	: "https://dev-finbazaar-admin.tk";
+	? "http://192.168.43.67:1401"
+	: "http://192.168.43.67:1401";
 // : // : "https://4fe85f9c.ngrok.io";
 
 export default {
-	LOGIN: BASE_URL + "/common/v0.1/login",
-	LOGOUT: BASE_URL + "/Session/v0.1/logout",
+	LOGIN: BASE_URL + "/Common/v0.1/login",
+	LOGOUT: BASE_URL + "/Common/v0.1/logout",
 
 	BASE_URL,
 
 	// // Auth End Points
-	// EXTENT_USER_SESSION: BASE_URL + "/Session/v0.1/extend-session",
+	// EXTEND_USER_SESSION: BASE_URL + "/Session/v0.1/extend-session",
 	// VERIFY_AND__UPDATE_USER_PASSWORD: BASE_URL + "/Mortal/v0.1/verify-mortal",
 
 };
