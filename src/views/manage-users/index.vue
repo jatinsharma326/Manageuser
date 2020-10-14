7e7f81cacb5ac2a7d30e9c65608477f1f62cc3d6
huzefakm <huzzwork@gmail.com>
2020-10-14 20:18:34 +0530

    Created usermanagement view

--- src/views/manage-users/index.vue
+++ src/views/manage-users/index.vue
@@ -1,12 +1,63 @@
 <template>
   <div class="">
-    Manage User Page 
+    <v-tabs icons-and-text grow v-model="tab">
+      <v-tab v-for="(ele, index) in tabConfig" :key="ele.id + '__' + index">{{
+        ele.name
+      }}</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item
+        v-for="(ele, index) in tabConfig"
+        :key="ele.id + '__' + index"
+      >
+        <!-- <component :dataProp="ele.props" v-bind:is="ele.component" /> -->
+        <Users :dataProp="ele.props"></Users>
+      </v-tab-item>
+    </v-tabs-items>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import Users from "./Users";
 export default {
-  name: 'ManageUsers',
-  components: {}
-}
+  name: "ManageUsers",
+  components: { Users },
+  data: () => ({
+    tab: "",
+    tabConfig: [
+      {
+        name: "Sales Team",
+        id: "salesTeam",
+        component: "Users",
+        props: {
+          type: "sales_agent",
+          placeholder: "Search Sales Team"
+        },
+      },
+      {
+        name: "Remote Sales Team",
+        id: "remoteSalesTeam",
+        component: "Users",
+        props: {
+          type: "remote_sales_agent",
+          placeholder: "Search Remote Sales Team"
+        },
+      },
+      {
+        name: "Management Team",
+        id: "management",
+        component: "Users",
+        props: {
+          type: "management",
+          placeholder: "Search Management Team"
+        },
+      },
+    ],
+  }),
+  computed: {
+    ...mapGetters([]),
+  },
+  methods: {},
+};
 </script>
