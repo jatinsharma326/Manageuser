2985a1ba5aa6f7859511d735ea26dda8600f993d
huzefakm <huzzwork@gmail.com>
2020-10-17 16:31:23 +0530

    add: API call for edit user and reset password

--- src/views/manage-users/store.js
+++ src/views/manage-users/store.js
@@ -63,11 +63,57 @@ export default {
         { root: true }
       )
         .then((data) => {
-          if (!data.ok) fail(data.message || "Failed to add master data rows");
+          if (!data.ok) fail(data.message || "Failed to add user");
           return data;
         })
         .catch((err) => {
-          fail(err.toString() || "Failed to add master data rows");
+          fail(err.toString() || "Failed to add user");
+          return {
+            ok: false,
+            message: err.message,
+          };
+        });
+    },
+    editUser: ({ commit, dispatch }, payload) => {
+      let fail = (msg) => commit("failure", msg);
+      return dispatch(
+        "apiCall",
+        {
+          method: "put",
+          data: payload,
+          url: constants.MANAGE_USERS,
+        },
+        { root: true }
+      )
+        .then((data) => {
+          if (!data.ok) fail(data.message || "Failed to edit user");
+          return data;
+        })
+        .catch((err) => {
+          fail(err.toString() || "Failed to edit user");
+          return {
+            ok: false,
+            message: err.message,
+          };
+        });
+    },
+    resetPassword: ({ commit, dispatch }, payload) => {
+      let fail = (msg) => commit("failure", msg);
+      return dispatch(
+        "apiCall",
+        {
+          method: "post",
+          data: payload,
+          url: constants.RESET_PASSWORD,
+        },
+        { root: true }
+      )
+        .then((data) => {
+          if (!data.ok) fail(data.message || "Failed to Reset Password");
+          return data;
+        })
+        .catch((err) => {
+          fail(err.toString() || "Failed to Reset Password");
           return {
             ok: false,
             message: err.message,
