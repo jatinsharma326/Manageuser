d30c7f9b4e3a28a3f959a369270ed7ca8483bd08
huzefakm <huzzwork@gmail.com>
2020-10-18 17:24:20 +0530

    create: Created Partner Store

---
+++ src/views/representing-partner/store.js
@@ -0,0 +1,128 @@
+import constants from "@/api";
+
+const initialState = () => ({});
+export default {
+  namespaced: true,
+  state: initialState(),
+  mutations: {
+    failure: (s, msg) => {
+      console.log("[failure] ", msg);
+    },
+    resetState(state) {
+      const initial = initialState();
+      Object.keys(initial).forEach((key) => {
+        state[key] = initial[key];
+      });
+    },
+  },
+  actions: {
+    // getUserList: ({ commit, dispatch }, payload) => {
+    //   let fail = (msg) => commit("failure", msg);
+    //   return dispatch(
+    //     "apiCall",
+    //     {
+    //       method: "get",
+    //       params: payload,
+    //       url: constants.MANAGE_USERS,
+    //     },
+    //     { root: true }
+    //   )
+    //     .then((data) => {
+    //       if (data.ok) {
+    //         return {
+    //           ok: true,
+    //           totalCount: data.totalCount,
+    //           fetchCount: data.fetchCount,
+    //           userList: data.data,
+    //         };
+    //       } else {
+    //         fail(data.message || "Failed to load User List");
+    //         return {
+    //           ok: false,
+    //           totalCount: data.totalCount,
+    //           fetchCount: 0,
+    //           userList: [],
+    //         };
+    //       }
+    //     })
+    //     .catch((err) => {
+    //       console.log("Yo ", err);
+    //       fail(err.toString() || "Failed to load User List");
+    //       return { ok: false, totalCount: 0, fetchCount: 0, userList: [] };
+    //     });
+    // },
+    // addUser: ({ commit, dispatch }, payload) => {
+    //   let fail = (msg) => commit("failure", msg);
+    //   return dispatch(
+    //     "apiCall",
+    //     {
+    //       method: "post",
+    //       data: payload,
+    //       url: constants.MANAGE_USERS,
+    //     },
+    //     { root: true }
+    //   )
+    //     .then((data) => {
+    //       if (!data.ok) fail(data.message || "Failed to add user");
+    //       return data;
+    //     })
+    //     .catch((err) => {
+    //       fail(err.toString() || "Failed to add user");
+    //       return {
+    //         ok: false,
+    //         message: err.message,
+    //       };
+    //     });
+    // },
+    // editUser: ({ commit, dispatch }, payload) => {
+    //   let fail = (msg) => commit("failure", msg);
+    //   return dispatch(
+    //     "apiCall",
+    //     {
+    //       method: "put",
+    //       data: payload,
+    //       url: constants.MANAGE_USERS,
+    //     },
+    //     { root: true }
+    //   )
+    //     .then((data) => {
+    //       if (!data.ok) fail(data.message || "Failed to edit user");
+    //       return data;
+    //     })
+    //     .catch((err) => {
+    //       fail(err.toString() || "Failed to edit user");
+    //       return {
+    //         ok: false,
+    //         message: err.message,
+    //       };
+    //     });
+    // },
+    // resetPassword: ({ commit, dispatch }, payload) => {
+    //   let fail = (msg) => commit("failure", msg);
+    //   return dispatch(
+    //     "apiCall",
+    //     {
+    //       method: "post",
+    //       data: payload,
+    //       url: constants.RESET_PASSWORD,
+    //     },
+    //     { root: true }
+    //   )
+    //     .then((data) => {
+    //       if (!data.ok) fail(data.message || "Failed to Reset Password");
+    //       return data;
+    //     })
+    //     .catch((err) => {
+    //       fail(err.toString() || "Failed to Reset Password");
+    //       return {
+    //         ok: false,
+    //         message: err.message,
+    //       };
+    //     });
+    // },
+  },
+  getters: {
+    // masterData: (state) => state.masterData,
+    // masterDataCount: (state) => state.masterDataCount,
+  },
+};
