0593039b8503df27cda3d80736124776e2478dc1
huzefakm <huzzwork@gmail.com>
2020-10-18 18:58:26 +0530

    Create: Create Partner Employees vue file

---
+++ src/views/representing-partner/PartnerEmployees.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="partnerEmployeesWrapper">
+    <v-card>
+      <v-toolbar dark color="primary">
+        <v-btn icon dark @click="closeModal">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-toolbar-title>Settings</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn dark text @click="dialog = false">
+            Save
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-card>
+  </div>
+</template>
+<script>
+import InformationCard from "../../components/InformationCard.vue";
+import Search from "../../components/Search.vue";
+
+export default {
+  name: "PartnerEmployee",
+  components: {
+    InformationCard,
+    Search,
+  },
+  created() {
+    // this.getUsers();
+  },
+  methods: {
+    closeModal() {
+      this.$emit("closeModal");
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.partnerEmployeesWrapper {
+  background-color: white;
+  height: 100%;
+}
+</style>
