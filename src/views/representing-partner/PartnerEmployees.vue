70b29cc8b10f9a36927b95166991bc2b1f8cf9ea
huzefakm <huzzwork@gmail.com>
2020-10-19 19:43:58 +0530

    create: Search config

--- src/views/representing-partner/PartnerEmployees.vue
+++ src/views/representing-partner/PartnerEmployees.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="partnerEmployeesWrapper">
-    <v-card>
-      <v-toolbar dark color="primary">
-        <v-btn icon dark @click="closeModal">
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-        <v-toolbar-title>{{ partnerInfo.name }}</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn dark text @click="dialog = false">
-            Save
-          </v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-    </v-card>
+    <v-row class="px-6" justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <Search
+          @queryString="queryString"
+          @filterObject="advanceSearch"
+          :placeholder="placeholder"
+          :isAdvanceSearch="true"
+          :filterConfig="selectedSearchConfig"
+        ></Search>
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script>
@@ -28,13 +25,68 @@ export default {
   },
   created() {
     // this.getUsers();
+    console.log("Partner Info", this.partnerInfo);
+    if (this.partnerInfo.countries) {
+      this.setSearchConfig(this.partnerInfo.countries);
+    }
   },
+  data: () => ({
+    search_text: "",
+    placeholder: "Search Employees",
+  }),
   methods: {
-    closeModal() {
-      this.$emit("closeModal");
+    queryString(data) {
+      this.search_text = data;
+      this.getUsers();
+    },
+    advanceSearch(filterObject) {
+      // make changes here to the filterObject
+      // this.getUsers();
+    },
+    setSearchConfig(countries = []) {
+      /*
+       * Name of Employee - Text field - string or number - can this be empty
+       * Designation of Employee - Text field - string or number - can this be empty
+       * Countries - Dropdown multi Autocomplete - need some default filter provision. - can be empty in this case but not in specific cases
+       */
+      this.selectedSearchConfig = [
+        {
+          name: "Employee Name",
+          key: "name",
+          type: "text",
+          inputType: "textfield",
+          defaultValue: "",
+        },
+        {
+          name: "Designation",
+          key: "designation",
+          type: "text",
+          inputType: "textfield",
+          defaultValue: "",
+        },
+        {
+          name: "Country",
+          key: "country",
+          multi: false,
+          inputType: "dropdown",
+          defaultValue: [],
+          isListInStore: false,
+          listItems: countries,
+        },
+      ];
     },
   },
-  watch: {},
+  watch: {
+    partnerInfo: {
+      deep: true,
+      handler(nv, ov) {
+        console.log("Handler");
+        if (nv.countries) {
+          this.setSearchConfig(nv.countries);
+        }
+      },
+    },
+  },
   props: {
     partnerInfo: { required: true, type: Object },
   },
