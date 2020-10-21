5ebb9838d45a3fe14fa6b58329f6271cca31c257
huzefakm <huzzwork@gmail.com>
2020-10-21 18:28:33 +0530

    add: default Partner list

--- src/views/manage-users/index.vue
+++ src/views/manage-users/index.vue
@@ -25,11 +25,14 @@ import {
   numeric,
   alpha,
 } from "vuelidate/lib/validators";
-import { mapGetters } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 import Users from "./Users";
 export default {
   name: "ManageUsers",
   components: { Users },
+  created() {
+    this.getPartnerList();
+  },
   data: () => ({
     tab: "",
     tabConfig: [
@@ -338,6 +341,8 @@ export default {
   computed: {
     ...mapGetters([]),
   },
-  methods: {},
+  methods: {
+    ...mapActions(["getPartnerList"]),
+  },
 };
 </script>
