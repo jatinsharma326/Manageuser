545aa8a572cfab7ba4350c08a8bd5ab87fbd42a2
huzefakm <huzzwork@gmail.com>
2020-10-21 14:50:03 +0530

    started working on get country

--- src/store/index.js
+++ src/store/index.js
@@ -1170,6 +1170,9 @@ export default new Vuex.Store({
       localStorage.setItem("auth", data.token);
       localStorage.setItem("userState", JSON.stringify(data.dataObj));
     },
+    // setCountries(state, { data }) {
+    //   state.countries;
+    // },
     loginFail: (s, p) => {
       s.messages.loginFailed = p;
     },
@@ -1383,6 +1386,41 @@ export default new Vuex.Store({
           return { ok: false };
         });
     },
+    // getCountries: ({ commit, dispatch }, payload) => {
+    //   let fail = (msg) => commit("failure", msg);
+    //   return dispatch(
+    //     "apiCall",
+    //     {
+    //       method: "get",
+    //       params: payload,
+    //       // url: constants.MANAGE_USERS,
+    //     },
+    //     { root: true }
+    //   )
+    //     .then((data) => {
+    //       if (data.ok) {
+    //         return {
+    //           ok: true,
+    //           // totalCount: data.totalCount,
+    //           // fetchCount: data.fetchCount,
+    //           // list: data.data,
+    //         };
+    //       } else {
+    //         fail(data.message || "Failed to load User List");
+    //         return {
+    //           ok: false,
+    //           // totalCount: data.totalCount,
+    //           // fetchCount: 0,
+    //           // list: [],
+    //         };
+    //       }
+    //     })
+    //     .catch((err) => {
+    //       console.log("Yo ", err);
+    //       fail(err.toString() || "Failed to fetch Countries List");
+    //       return { ok: false, totalCount: 0, fetchCount: 0, list: [] };
+    //     });
+    // },
   },
 
   getters: {
