64fdafcf4b11170a1add2f11f5b2de2b70281af2
huzefakm <huzzwork@gmail.com>
2020-10-21 18:24:22 +0530

    add: API end point for Countries, Zone, Partners, Business Type List

--- src/api/index.js
+++ src/api/index.js
@@ -15,6 +15,10 @@ export default {
   // VERIFY_AND__UPDATE_USER_PASSWORD: BASE_URL + "/Mortal/v0.1/verify-mortal",
 
   RESET_PASSWORD: BASE_URL + "Admin/v0.1/reset-password",
+  ALL_COUNTRIES: BASE_URL + "Global/v0.1/countries",
+  ALL_PARTNERS: BASE_URL + "Global/v0.1/representing-partners",
+  ALL_ZONE: BASE_URL + "Global/v0.1/zones",
+  ALL_BUSINESS_TYPE: BASE_URL + "Global/v0.1/business-types",
 
   //API ENDPOINT FOR MANAGE USER DATA
   MANAGE_USERS: BASE_URL + "Mortals/v0.1/",
