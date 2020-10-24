ff44eb958b8434366f507b4f68dd1759274f8b97
taherPardawala <skypunch2@gmail.com>
2020-10-24 18:54:00 +0530

    worked on accept reject functionality

--- src/views/leave-manager/index.vue
+++ src/views/leave-manager/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="partnerEmployeesWrapper">
+	<div class="leavesManagerWrapper">
 		<v-row class="px-6 employee-search-bar" justify="center" align="center">
 			<v-col cols="12" sm="8" md="6">
 				<Search
@@ -11,13 +11,46 @@
 				></Search>
 			</v-col>
 		</v-row>
+		<div class="leaves-table">
+			<v-data-table :headers="headers" :items="leavesList" sort-by="" class="elevation-1">
+				<template v-slot:item.doa="{ item }">
+					{{ getFormattedDate(item.doa, "MMMM Do YYYY dddd") }}
+				</template>
+				<template v-slot:item.date_from="{ item }">
+					{{ getFormattedDate(item.date_from, "MMMM Do YYYY dddd") }}
+				</template>
+				<template v-slot:item.date_to="{ item }">
+					{{ getFormattedDate(item.date_to, "MMMM Do YYYY dddd") }}
+				</template>
+				<template v-slot:item.actions="{ item }">
+					<template v-if="item.status != 'REJECTED' && isDateBefore(item.date_from)">
+						<v-menu bottom left>
+							<template v-slot:activator="{ on, attrs }">
+								<v-btn icon v-bind="attrs" v-on="on">
+									<v-icon>mdi-dots-vertical</v-icon>
+								</v-btn>
+							</template>
+
+							<v-list>
+								<v-list-item v-if="item.status == 'ACCEPTED'" @click="acceptAction(item)"
+									>Accept</v-list-item
+								>
+								<v-list-item @click="rejectAction(item)">Reject</v-list-item>
+							</v-list>
+						</v-menu>
+					</template>
+				</template>
+			</v-data-table>
+		</div>
 	</div>
 </template>
 
 <script>
 	import defaultCRUDMixin from "../../mixins/defaultCRUDMixins";
+	import helpers from "../../components/helpers";
 	import { required, email, minLength, numeric, alpha } from "vuelidate/lib/validators";
 	import { mapActions, mapGetters, mapMutations } from "vuex";
+	import moment from "moment-timezone";
 
 	export default {
 		name: "LeaveManager",
@@ -32,16 +65,38 @@
 		data: () => ({
 			leavesList: [],
 			countriesWithEmployee: [],
+			headers: [
+				{
+					text: "Applicant",
+					align: "start",
+					sortable: false,
+					value: "name",
+				},
+				{ text: "Date of Application", value: "doa" },
+				{ text: "Date From", value: "date_from" },
+				{ text: "Date To", value: "date_to" },
+				{ text: "No of Days", value: "no_of_days" },
+				{ text: "Pending Leaves", value: "pending_leaves" },
+				{ text: "Status", value: "status" },
+				{ text: "", value: "actions" },
+			],
 			search_text: "",
 			placeholder: "Search Leaves",
 		}),
 		methods: {
 			...mapActions("LeaveManager", [
 				"getAllLeaves",
+				"updateStatus",
 				/* "addPartnerEmployees",
-				"editPartnerEmployees",
 				"deletePartnerEmployees", */
 			]),
+			isDateBefore(date) {
+				if (moment().isBefore(date)) {
+					return true;
+				} else {
+					return false;
+				}
+			},
 			getData() {
 				this.openLoaderDialog();
 				// this.filter.representing_partner_id = this.partnerInfo._id;
@@ -55,6 +110,42 @@
 					this.totalCount = data.totalCount;
 					this.fetchCount = data.fetchCount;
 					console.log(this.leavesList);
+					for (let listItem of this.leavesList) {
+					}
+				});
+			},
+			acceptAction(leave) {
+				this.openLoaderDialog();
+				this.updateStatus({
+					_id: leave._id,
+					status: "ACCEPTED",
+					updated_on: leave.record.updated_on,
+					pending_leaves: leave.pending_leaves,
+				}).then((data) => {
+					this.closeLoaderDialog();
+					if (data.ok) {
+						this.openSnackbar({ text: "Sucessfully Updated Leave Status" });
+						this.getData();
+					} else {
+						this.openSnackbar({ text: data.message });
+					}
+				});
+			},
+			rejectAction(leave) {
+				this.openLoaderDialog();
+				this.updateStatus({
+					_id: leave._id,
+					status: "REJECTED",
+					updated_on: leave.record.updated_on,
+					pending_leaves: leave.pending_leaves,
+				}).then((data) => {
+					this.closeLoaderDialog();
+					if (data.ok) {
+						this.openSnackbar({ text: "Sucessfully Updated Leave Status" });
+						this.getData();
+					} else {
+						this.openSnackbar({ text: data.message });
+					}
 				});
 			},
 			queryString(data) {
