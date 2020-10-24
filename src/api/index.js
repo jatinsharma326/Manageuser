9af933468de36e1bcdee64bd9812a5f65fddd6af
taherPardawala <skypunch2@gmail.com>
2020-10-24 16:35:20 +0530

    API End point for Sales Leave

--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,31 @@
 const isBuildVersion = process.env.NODE_ENV === "production";
 
-const BASE_URL = isBuildVersion
-  ? "https://globaldestinations.tk/"
-  : "https://globaldestinations.tk/";
+const BASE_URL = isBuildVersion ? "https://globaldestinations.tk/" : "https://globaldestinations.tk/";
 // : // : "https://4fe85f9c.ngrok.io";
 
 export default {
-  LOGIN: BASE_URL + "Common/v0.1/login",
-  LOGOUT: BASE_URL + "Common/v0.1/logout",
-  BASE_URL,
+	LOGIN: BASE_URL + "Common/v0.1/login",
+	LOGOUT: BASE_URL + "Common/v0.1/logout",
+	BASE_URL,
 
-  EXTEND_USER_SESSION: BASE_URL + "Session/v0.1/extend-session",
-  // // Auth End Points
-  // VERIFY_AND__UPDATE_USER_PASSWORD: BASE_URL + "/Mortal/v0.1/verify-mortal",
+	EXTEND_USER_SESSION: BASE_URL + "Session/v0.1/extend-session",
+	// // Auth End Points
+	// VERIFY_AND__UPDATE_USER_PASSWORD: BASE_URL + "/Mortal/v0.1/verify-mortal",
 
-  RESET_PASSWORD: BASE_URL + "Admin/v0.1/reset-password",
-  ALL_COUNTRIES: BASE_URL + "Global/v0.1/countries",
-  ALL_PARTNERS: BASE_URL + "Global/v0.1/representing-partners",
-  ALL_ZONE: BASE_URL + "Global/v0.1/zones",
-  ALL_BUSINESS_TYPE: BASE_URL + "Global/v0.1/business-types",
-  ALL_CURRENCIES: BASE_URL + "Global/v0.1/currencies",
+	RESET_PASSWORD: BASE_URL + "Admin/v0.1/reset-password",
+	ALL_COUNTRIES: BASE_URL + "Global/v0.1/countries",
+	ALL_PARTNERS: BASE_URL + "Global/v0.1/representing-partners",
+	ALL_ZONE: BASE_URL + "Global/v0.1/zones",
+	ALL_BUSINESS_TYPE: BASE_URL + "Global/v0.1/business-types",
+	ALL_CURRENCIES: BASE_URL + "Global/v0.1/currencies",
 
-  //API ENDPOINT FOR MANAGE USER DATA
-  MANAGE_USERS: BASE_URL + "Mortals/v0.1/",
-  MANAGE_PARTNER: BASE_URL + "Partners/v0.1/",
-  MANAGE_PARTNER_EMPLOYEES: BASE_URL + "RepEmployees/v0.1/",
+	//API ENDPOINT FOR MANAGE USER DATA
+	MANAGE_USERS: BASE_URL + "Mortals/v0.1/",
+	MANAGE_PARTNER: BASE_URL + "Partners/v0.1/",
+	MANAGE_PARTNER_EMPLOYEES: BASE_URL + "RepEmployees/v0.1/",
 
-  GLOBAL_SETTINGS: BASE_URL + "Admin/v0.1/global-settings",
-  GLOBAL_SETTINGS_POLICIES: BASE_URL + "Admin/v0.1/policies",
+	GLOBAL_SETTINGS: BASE_URL + "Admin/v0.1/global-settings",
+	GLOBAL_SETTINGS_POLICIES: BASE_URL + "Admin/v0.1/policies",
+
+	ALL_SALES_LEAVES: BASE_URL + "Leaves/v0.1/all-sales-leaves",
 };
