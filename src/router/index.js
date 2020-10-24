b09adaf5a688eadb7f2a4bfb8d0db54efa020bc1
taherPardawala <skypunch2@gmail.com>
2020-10-24 16:35:55 +0530

    add: route for Leaves Manager

--- src/router/index.js
+++ src/router/index.js
@@ -4,28 +4,34 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/manage-users",
-    name: "Manage Users",
-    component: () => import("../views/manage-users"),
-    meta: { policy: [10, 20, 30, 40] },
-  },
-  {
-    path: "/regions-and-partners",
-    name: "Regions and Partners",
-    component: () => import("../views/representing-partner"),
-    meta: { policy: [10, 20, 30, 40] },
-  },
-  {
-    path: "/settings",
-    name: "Settings",
-    component: () => import("../views/settings"),
-    meta: { policy: [10, 20, 30, 40] },
-  },
+	{
+		path: "/manage-users",
+		name: "Manage Users",
+		component: () => import("../views/manage-users"),
+		meta: { policy: [10, 20, 30, 40] },
+	},
+	{
+		path: "/regions-and-partners",
+		name: "Regions and Partners",
+		component: () => import("../views/representing-partner"),
+		meta: { policy: [10, 20, 30, 40] },
+	},
+	{
+		path: "/settings",
+		name: "Settings",
+		component: () => import("../views/settings"),
+		meta: { policy: [10, 20, 30, 40] },
+	},
+	{
+		path: "/leaves",
+		name: "Leave Manager",
+		component: () => import("../views/leave-manager"),
+		meta: { policy: [10, 20, 30, 40] },
+	},
 ];
 
 const router = new VueRouter({
-  routes,
+	routes,
 });
 
 export default router;
