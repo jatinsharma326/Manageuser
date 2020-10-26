ebbe77064eaa5a4ef59794006e0dd87be3a5bf6a
huzefakm <huzzwork@gmail.com>
2020-10-26 20:00:22 +0530

    add: functionality to add year

--- src/views/manage-targets/index.vue
+++ src/views/manage-targets/index.vue
@@ -1,101 +1,51 @@
 <template>
 	<div class="targetsWrapper">
 		<div class="card-wrapper">
-			<div v-for="user in targetList" :key="user._id" class="card-element">
-				<InformationCard :expandCard="true">
+			<div v-for="target in targetList" :key="target._id" class="card-element">
+				<InformationCard>
 					<template v-slot:topLeft>
-						{{ user.business_types.join(", ") }}
+						Targets for Year
 					</template>
 					<template v-slot:mainContent>
-						{{ user.name }}
-					</template>
-					<template v-slot:mainContentSubtitle>
-						{{ user.proprietor_info }}
-					</template>
-					<template v-slot:mainContentRight>
-						<div class="card-image">
-							<img :src="user.logo" alt="" />
-						</div>
-					</template>
-					<template v-slot:moreInfo>
-						{{ user.countries.join(", ") }}
+						{{ target.year }}
 					</template>
 					<template v-slot:actionButtons>
 						<template>
-							<v-btn
-								v-if="userType == ADMIN || userType == MANAGEMENT"
-								@click="disablePartner(user)"
-								color="error"
-								text
-							>
-								{{ user.record.active ? "Disable" : "Enable" }}
+							<v-btn @click="disablePartner(target)" color="error" text>
+								Delete
 							</v-btn>
-							<v-btn
-								v-if="userType == ADMIN || userType == MANAGEMENT"
-								@click="openInputForm(true, user)"
-								color="secondary lighten-2"
-								text
-							>
+							<v-btn @click="openInputForm(true, target)" color="secondary lighten-2" text>
 								Edit
 							</v-btn>
-							<v-btn @click="openTargetsModal(user)" color="primary lighten-2" text>
+							<v-btn @click="openTargetsModal(target)" color="primary lighten-2" text>
 								View
 							</v-btn>
 						</template>
-					</template>
-					<template v-slot:expandCardContent>
-						<v-list>
-							<v-list-item
-								v-for="(contact, index) in user.emergency_contacts"
-								:key="user._id + '+' + index"
-								two-line
-							>
-								<v-list-item-icon>
-									<v-icon color="indigo">
-										mdi-phone
-									</v-icon>
-								</v-list-item-icon>
-
-								<v-list-item-content>
-									<v-list-item-title>{{ contact.contacts.join(", ") }}</v-list-item-title>
-									<v-list-item-subtitle>{{ contact.country }}</v-list-item-subtitle>
-								</v-list-item-content>
-								<!-- <v-divider inset></v-divider> -->
-							</v-list-item>
-						</v-list>
 					</template>
 				</InformationCard>
 			</div>
 		</div>
 
 		<ViewMoreModal @closeModal="viewMoreModal = false" :toggleModal="viewMoreModal">
-			<!-- <template v-slot:modalTitle>
-				<div v-if="selectedPartnerInfo.name">
-					{{ selectedPartnerInfo.name }}
-				</div>
+			<template v-slot:modalTitle>
+				<div v-if="selectedTarget.year">Targets for the Year {{ selectedTarget.year }}</div>
 			</template>
-			<template v-slot:modalSubtitle>
-				<div v-if="selectedPartnerInfo.business_types">
-					{{ selectedPartnerInfo.business_types.join("/ ") }}
-				</div>
-			</template> -->
 			<template v-slot:modalContent>
-				<!-- <partnerEmployees :partnerInfo="selectedPartnerInfo"></partnerEmployees> -->
+				<ProductTargets :targetYear="selectedTarget"></ProductTargets>
 			</template>
 		</ViewMoreModal>
 
-		<!-- <UserForm
+		<UserForm
 			@formOutput="formOutput"
 			@closeForm="closeForm"
 			:name="name"
 			:inputConfig="inputConfig"
-			:keysToWatch="keysToWatch"
 			:toggleForm="toggleForm"
 			:formData="rowToEdit"
 			:isEditMode="isEditMode"
-		></UserForm> -->
+		></UserForm>
 
-		<div v-if="userType == ADMIN || userType == MANAGEMENT" class="floating-button">
+		<div class="floating-button">
 			<v-btn @click="openInputForm()" color="primary" dark fab>
 				<v-icon>mdi-plus</v-icon>
 			</v-btn>
@@ -105,161 +55,192 @@
 
 <script>
 	import defaultCRUDMixin from "../../mixins/defaultCRUDMixins";
-	import { required, email, minLength, numeric, alpha } from "vuelidate/lib/validators";
+	import { required } from "vuelidate/lib/validators";
 	import { mapActions, mapGetters, mapMutations } from "vuex";
 	import helpers from "../../components/helpers";
 	import ViewMoreModal from "../../components/ViewMoreModal";
+	import ProductTargets from "./ProductTargets";
 
 	export default {
 		name: "ManageTargets",
 		mixins: [defaultCRUDMixin],
-		components: {},
-		created() {
-			// this.getPartners();
+		components: {
+			ProductTargets,
 		},
-		data: () => ({
-			targetList: [],
-			name: "Year",
-			inputConfig: [
-				{
-					name: "Partner Name",
-					type: "String",
-					key: "name",
-					width: "half",
-					validations: {
-						required,
-						minLength: minLength(1),
+		created() {
+			this.getYearlyTargets();
+		},
+		data: () => {
+			let years = [
+				2000,
+				2001,
+				2002,
+				2003,
+				2004,
+				2005,
+				2006,
+				2007,
+				2008,
+				2009,
+				2010,
+				2011,
+				2012,
+				2013,
+				2014,
+				2015,
+				2016,
+				2017,
+				2018,
+				2019,
+				2020,
+				2021,
+				2022,
+				2023,
+				2024,
+				2025,
+				2026,
+				2027,
+				2028,
+				2029,
+				2030,
+				2031,
+				2032,
+				2033,
+				2034,
+				2035,
+				2036,
+				2037,
+				2038,
+				2039,
+				2040,
+				2041,
+				2042,
+				2043,
+				2044,
+				2045,
+				2046,
+				2047,
+				2048,
+				2049,
+				2050,
+				2051,
+				2052,
+				2053,
+				2054,
+				2055,
+				2056,
+				2057,
+				2058,
+				2059,
+				2060,
+				2061,
+				2062,
+				2063,
+				2064,
+				2065,
+				2066,
+				2067,
+				2068,
+				2069,
+				2070,
+				2071,
+				2072,
+				2073,
+				2074,
+				2075,
+				2076,
+				2077,
+				2078,
+				2079,
+				2080,
+				2081,
+				2082,
+				2083,
+				2084,
+				2085,
+				2086,
+				2087,
+				2088,
+				2089,
+				2090,
+				2091,
+				2092,
+				2093,
+				2094,
+				2095,
+				2096,
+				2097,
+				2098,
+				2099,
+				2100,
+			];
+			return {
+				targetList: [],
+				name: "Year",
+				selectedTarget: {},
+				inputConfig: [
+					{
+						name: "Target Year",
+						type: "Dropdown",
+						key: "year",
+						width: "half",
+						multi: false,
+						isListInStore: false,
+						listItems: years,
+						validations: {
+							required,
+						},
 					},
-				},
-				{
-					name: "Proprietor Info",
-					type: "String",
-					key: "proprietor_info",
-					width: "full",
-					validations: {
-						required,
-						minLength: minLength(1),
+					{
+						name: "Duplicate Targets From Year",
+						type: "Dropdown",
+						key: "reference_year",
+						width: "half",
+						multi: false,
+						isListInStore: false,
+						listItems: years,
 					},
-				},
-				{
-					name: "Countries",
-					type: "Dropdown",
-					key: "countries",
-					width: "half",
-					multi: true,
-					isListInStore: true,
-					listVariable: "countries",
-					validations: {
-						required,
-					},
-				},
-				{
-					name: "Business Type",
-					type: "Dropdown",
-					key: "business_types",
-					width: "half",
-					multi: true,
-					isListInStore: true,
-					listVariable: "businessType",
-					validations: {
-						required,
-					},
-				},
-				{
-					name: "Partner Logo",
-					type: "FilePicker",
-					key: "logo",
-					width: "half",
-					acceptRules: "image/png, image/jpeg, image/bmp",
-					rules: [
-						(value) =>
-							!value || value.size <= 100000 || "Partner logo should be less than or equal to 100 kb!",
-					],
-				},
-				{
-					name: "Emergency Numbers",
-					type: "MultiInputWithGroupKey",
-					key: "emergency_contacts",
-					width: "half",
-					keyToGroup: "countries",
-				},
-			],
-		}),
+				],
+			};
+		},
 		computed: {},
 		methods: {
-			...mapActions("ManageTargets", ["getPartnerList", "addPartner", "editPartner"]),
-			getPartners() {
+			...mapActions("ManageTargets", ["getTargets", "addTargetYear", "editTargetYear"]),
+			getYearlyTargets() {
 				this.openLoaderDialog();
-				this.getPartnerList({
+				this.getTargets({
 					filter: this.filter,
 					pageSize: this.pageSize,
 					pageNo: this.pageNo,
 				}).then((data) => {
 					this.closeLoaderDialog();
 					this.targetList = data.list;
-					this.totalCount = data.totalCount;
-					this.fetchCount = data.fetchCount;
 				});
 			},
 			async formOutput(data) {
-				console.log("Before Cleaning", data);
-				var tempFile = data.logo;
 				var formData = JSON.parse(JSON.stringify(data));
-				formData.logo = tempFile;
-				console.log("After Cleaning", formData);
-				var tempArray = [];
-				var tempObj = {};
-
-				// loop over the emergency contacts objects to convert it into theh backend format
-				for (let contact of formData.emergency_contacts) {
-					tempObj = {};
-					for (let num of contact.input) {
-						if (num.input != "") {
-							tempObj["country"] = contact.groupKey;
-							if (!tempObj["contacts"]) tempObj["contacts"] = [];
-							tempObj["contacts"].push(num.input);
-						}
-					}
-					if (Object.keys(tempObj).length) {
-						tempArray.push(tempObj);
-					}
-				}
-				formData.emergency_contacts = tempArray;
-
-				// remove logo key if it's empty
-				if (formData.logo) {
-					formData.logo = await helpers.toBase64(formData.logo);
-				} else {
-					delete formData.logo;
-				}
-
-				console.log("Before API call FormData Object", formData);
 
 				this.openLoaderDialog();
 				if (!this.isEditMode) {
 					this.addPartner(formData).then((data) => {
 						this.closeLoaderDialog();
 						if (data.ok) {
-							this.openSnackbar({ text: "Sucessfully Added Partner" });
-							console.log("Add Partner success");
-							this.getPartners();
+							this.openSnackbar({ text: "Sucessfully Added Target" });
+							this.getYearlyTargets();
 							this.closeForm();
 						} else {
 							this.openSnackbar({ text: data.message });
-							console.log("Add Partner failed");
 						}
 					});
 				} else {
 					this.editPartner(formData).then((data) => {
 						this.closeLoaderDialog();
 						if (data.ok) {
-							this.openSnackbar({ text: "Sucessfully Edited Partner" });
-							console.log("Edit Partner success");
-							this.getPartners();
+							this.openSnackbar({ text: "Sucessfully Edited Target" });
+							this.getYearlyTargets();
 							this.closeForm();
 						} else {
 							this.openSnackbar({ text: data.message });
-							console.log("Edit Partner failed");
 						}
 					});
 				}
@@ -287,7 +268,7 @@
 						if (data.ok) {
 							this.openSnackbar({ text: "Sucessfully Updated Partner Status" });
 							console.log("Updated Partner status");
-							this.getPartners();
+							this.getYearlyTargets();
 							this.closeForm();
 						} else {
 							this.openSnackbar({ text: data.message });
@@ -296,9 +277,8 @@
 					});
 				}
 			},
-			openTargetsModal(userData) {
-				this.selectedPartnerInfo = { ...userData };
-				// console.log(this.selectedPartnerInfo);
+			openTargetsModal(targetData) {
+				this.selectedTarget = targetData;
 				this.viewMoreModal = true;
 			},
 		},
