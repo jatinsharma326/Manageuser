7e873bddbbe629a932c962f1e043880661eadac5
huzefakm <huzzwork@gmail.com>
2020-10-28 20:02:41 +0530

    add: Create files for Manage Travel Agent section

---
+++ src/views/manage-agents/CompanyInfo.vue
@@ -0,0 +1,467 @@
+<template>
+	<div class="partnerEmployeesWrapper">
+		<v-row class="px-6 employee-search-bar" justify="center" align="center">
+			<v-col cols="12" sm="8" md="6">
+				<Search
+					@queryString="queryString"
+					@filterObject="advanceSearch"
+					:placeholder="placeholder"
+					:isAdvanceSearch="true"
+					:filterConfig="selectedSearchConfig"
+				></Search>
+			</v-col>
+		</v-row>
+
+		<template v-if="employeesList.length">
+			<template v-for="(country, countryIndex) in countriesWithEmployee">
+				<div :key="country + '__' + countryIndex + '__tilte'" class="country-title">
+					{{ country }}
+				</div>
+				<div :key="country + '__' + countryIndex" class="card-wrapper">
+					<template v-for="user in employeesList">
+						<div v-if="country == user.country" class="card-element" :key="user._id">
+							<InformationCard :expandCard="true">
+								<template v-slot:mainContent>
+									{{ user.name }}
+								</template>
+								<template v-slot:mainContentSubtitle>
+									{{ user.designation }}
+								</template>
+								<template v-slot:moreInfo>
+									{{ user.country }}
+								</template>
+								<template v-slot:actionButtons>
+									<template v-if="userType == ADMIN || userType == MANAGEMENT">
+										<v-btn @click="deleteEmployee(user)" color="error" text>
+											Delete
+										</v-btn>
+										<v-btn @click="openInputForm(true, user)" color="secondary" text>
+											Edit
+										</v-btn>
+									</template>
+								</template>
+								<template v-slot:expandCardContent>
+									<v-list>
+										<v-list-item
+											v-for="(number, index) in user.phone_numbers"
+											:key="user._id + '+' + index"
+										>
+											<v-list-item-icon>
+												<v-icon v-if="index == 0" color="secondary">
+													mdi-phone
+												</v-icon>
+											</v-list-item-icon>
+
+											<v-list-item-content>
+												<v-list-item-title>{{ number }}</v-list-item-title>
+											</v-list-item-content>
+										</v-list-item>
+
+										<v-divider inset></v-divider>
+
+										<v-list-item
+											v-for="(email, index) in user.email_ids"
+											:key="user._id + '+' + index + 'Email'"
+										>
+											<v-list-item-icon>
+												<v-icon v-if="index == 0" color="secondary">
+													mdi-email
+												</v-icon>
+											</v-list-item-icon>
+
+											<v-list-item-content>
+												<v-list-item-title>{{ email }}</v-list-item-title>
+											</v-list-item-content>
+										</v-list-item>
+									</v-list>
+								</template>
+							</InformationCard>
+						</div>
+					</template>
+				</div>
+			</template>
+		</template>
+
+		<UserForm
+			@formOutput="formOutput"
+			@closeForm="closeForm"
+			:name="name"
+			:inputConfig="inputConfig"
+			:toggleForm="toggleForm"
+			:formData="rowToEdit"
+			:isEditMode="isEditMode"
+		></UserForm>
+
+		<div v-if="userType == ADMIN || userType == MANAGEMENT" class="floating-button">
+			<v-btn @click="openInputForm()" color="primary" dark fab>
+				<v-icon>mdi-plus</v-icon>
+			</v-btn>
+		</div>
+	</div>
+</template>
+
+<script>
+	import defaultCRUDMixin from "../../mixins/defaultCRUDMixins";
+	import { required, email, minLength, numeric, alpha } from "vuelidate/lib/validators";
+	import { mapActions, mapGetters, mapMutations } from "vuex";
+
+	export default {
+		name: "PartnerEmployee",
+		mixins: [defaultCRUDMixin],
+		created() {
+			this.getEmployees();
+			if (this.partnerInfo.countries) {
+				this.setSearchConfig(this.partnerInfo.countries);
+				this.setInputConfig(this.partnerInfo.countries);
+			}
+		},
+		data: () => ({
+			employeesList: [
+				// {
+				//   _id: "5f8e8f36bbaf403c3cf40e47",
+				//   representing_partner_id: "5f857a9ad8a96c2e4ca6e7c5",
+				//   name: "Sachin Tendulkar",
+				//   designation: "SS",
+				//   country: "United States",
+				//   phone_numbers: ["123", "321300"],
+				//   email_id: ["eassa@tese.com", "fasda@fcsa.in"],
+				//   record: {
+				//     created_on: "2020-10-20T07:18:14.082Z",
+				//     updated_on: "2020-10-20T07:18:14.082Z",
+				//     active: true,
+				//   },
+				// },
+				// {
+				//   _id: "5f8e8f36bbaf403c3cf41e47",
+				//   representing_partner_id: "5f857a9ad8a96c2e4ca6e7c5",
+				//   name: "Sachin Tendulkar",
+				//   designation: "SS",
+				//   country: "Egypt",
+				//   phone_numbers: ["123", "321300"],
+				//   email_id: ["eassa@tese.com", "fasda@fcsa.in"],
+				//   record: {
+				//     created_on: "2020-10-20T07:18:14.082Z",
+				//     updated_on: "2020-10-20T07:18:14.082Z",
+				//     active: true,
+				//   },
+				// },
+				// {
+				//   _id: "5f8e8f36bbaf413c3cf40e47",
+				//   representing_partner_id: "5f857a9ad8a96c2e4ca6e7c5",
+				//   name: "Sachin Tendulkar",
+				//   designation: "SS",
+				//   country: "United States",
+				//   phone_numbers: ["123", "321300"],
+				//   email_id: ["eassa@tese.com", "fasda@fcsa.in"],
+				//   record: {
+				//     created_on: "2020-10-20T07:18:14.082Z",
+				//     updated_on: "2020-10-20T07:18:14.082Z",
+				//     active: true,
+				//   },
+				// },
+			],
+			countriesWithEmployee: [],
+			search_text: "",
+			placeholder: "Search Employees",
+			name: "Representing Partner Employees",
+			inputConfig: [
+				{
+					name: "Employee Name*",
+					type: "String",
+					key: "name",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+					},
+				},
+				{
+					name: "Designation*",
+					type: "String",
+					key: "designation",
+					width: "full",
+					validations: {
+						required,
+						minLength: minLength(1),
+					},
+				},
+				{
+					name: "Country*",
+					type: "Dropdown",
+					key: "country",
+					width: "half",
+					multi: false,
+					listVariable: "countries",
+					isListInStore: false,
+					// listItems: countries,
+					validations: {
+						required,
+					},
+				},
+				{
+					name: "Contact Info*",
+					type: "MultiInput",
+					key: "phone_numbers",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+						$each: {
+							input: {
+								required,
+							},
+						},
+					},
+				},
+				{
+					name: "Email*",
+					type: "MultiInput",
+					key: "email_ids",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+						$each: {
+							input: {
+								required,
+							},
+						},
+					},
+				},
+			],
+		}),
+		methods: {
+			...mapActions("PartnerManagement", [
+				"getPartnerEmployeesList",
+				"addPartnerEmployees",
+				"editPartnerEmployees",
+				"deletePartnerEmployees",
+			]),
+			getEmployees() {
+				this.openLoaderDialog();
+				this.filter.representing_partner_id = this.partnerInfo._id;
+				this.getPartnerEmployeesList({
+					filter: this.filter,
+					pageSize: this.pageSize,
+					pageNo: this.pageNo,
+				}).then((data) => {
+					this.closeLoaderDialog();
+					this.employeesList = data.list;
+					this.totalCount = data.totalCount;
+					this.fetchCount = data.fetchCount;
+					let temp = this.employeesList.map((e) => e.country);
+					this.countriesWithEmployee = Array.from(new Set(temp));
+				});
+			},
+			queryString(data) {
+				this.filter["search_text"] = data;
+				this.getEmployees();
+			},
+			advanceSearch(filterObject) {
+				// make changes here to the filterObject
+				this.filter = { ...filterObject };
+				this.pageNo = 1;
+				this.getEmployees();
+			},
+			setSearchConfig(countries = []) {
+				/*
+				 * Name of Employee - Text field - string or number - can this be empty
+				 * Designation of Employee - Text field - string or number - can this be empty
+				 * Countries - Dropdown multi Autocomplete - need some default filter provision. - can be empty in this case but not in specific cases
+				 */
+				this.selectedSearchConfig = [
+					{
+						name: "Employee Name",
+						key: "name",
+						type: "text",
+						inputType: "textfield",
+						defaultValue: "",
+					},
+					{
+						name: "Designation",
+						key: "designation",
+						type: "text",
+						inputType: "textfield",
+						defaultValue: "",
+					},
+					{
+						name: "Country",
+						key: "country",
+						multi: false,
+						inputType: "dropdown",
+						defaultValue: [],
+						isListInStore: false,
+						listItems: countries,
+					},
+				];
+			},
+			setInputConfig(countries = []) {
+				this.inputConfig = [
+					{
+						name: "Employee Name*",
+						type: "String",
+						key: "name",
+						width: "half",
+						validations: {
+							required,
+							minLength: minLength(1),
+						},
+					},
+					{
+						name: "Designation*",
+						type: "String",
+						key: "designation",
+						width: "half",
+						validations: {
+							required,
+							minLength: minLength(1),
+						},
+					},
+					{
+						name: "Country*",
+						type: "Dropdown",
+						key: "country",
+						width: "full",
+						multi: false,
+						isListInStore: false,
+						listItems: countries,
+						validations: {
+							required,
+						},
+					},
+					{
+						name: "Contact Info*",
+						type: "MultiInput",
+						key: "phone_numbers",
+						width: "half",
+						validations: {
+							required,
+							minLength: minLength(1),
+							$each: {
+								input: {
+									required,
+								},
+							},
+						},
+					},
+					{
+						name: "Email*",
+						type: "MultiInput",
+						key: "email_ids",
+						width: "half",
+						validations: {
+							required,
+							minLength: minLength(1),
+							$each: {
+								input: {
+									email,
+									required,
+								},
+							},
+						},
+					},
+				];
+			},
+			formOutput(data) {
+				var formData = JSON.parse(JSON.stringify(data));
+				// var tempArray = [];
+				// var tempObj = {};
+				formData.phone_numbers = data.phone_numbers.map((data) => data.input);
+				formData.email_ids = data.email_ids.map((data) => data.input);
+				console.log("Before API call FormData Object", formData);
+
+				this.openLoaderDialog();
+				if (!this.isEditMode) {
+					formData.representing_partner_id = this.partnerInfo._id;
+					this.addPartnerEmployees(formData).then((data) => {
+						this.closeLoaderDialog();
+						if (data.ok) {
+							this.openSnackbar({ text: "Sucessfully Added Employee Info" });
+							console.log("Add Partner Employee success");
+							this.getEmployees();
+							this.closeForm();
+						} else {
+							this.openSnackbar({ text: data.message });
+							console.log("Add Partner Employee failed");
+						}
+					});
+				} else {
+					this.editPartnerEmployees(formData).then((data) => {
+						this.closeLoaderDialog();
+						if (data.ok) {
+							this.openSnackbar({ text: "Sucessfully Edited Employee Info" });
+							console.log("Edit Partner Employee success");
+							this.getEmployees();
+							this.closeForm();
+						} else {
+							this.openSnackbar({ text: data.message });
+							console.log("Edit Partner Employee failed");
+						}
+					});
+				}
+			},
+			getEditRowObject(data) {
+				return {
+					...data,
+					_id: data._id,
+					updated_on: data.record.updated_on,
+				};
+			},
+
+			// Implement the delete API
+			deleteEmployee(user) {
+				console.log(user);
+				if (window.confirm("Do you really want to Delete the Partner Employee?")) {
+					this.openLoaderDialog();
+					this.deletePartnerEmployees({
+						_id: user._id,
+					}).then((data) => {
+						this.closeLoaderDialog();
+						if (data.ok) {
+							this.openSnackbar({ text: "Sucessfully Deleted Employee" });
+							this.getEmployees();
+						} else {
+							this.openSnackbar({ text: data.message });
+						}
+					});
+				}
+			},
+		},
+		watch: {
+			partnerInfo: {
+				deep: true,
+				handler(nv, ov) {
+					console.log("Handler");
+					this.filter = {};
+					this.employeesList = [];
+					this.getEmployees();
+					if (nv.countries) {
+						this.setSearchConfig(nv.countries);
+						this.setInputConfig(this.partnerInfo.countries);
+					}
+				},
+			},
+		},
+		props: {
+			partnerInfo: { required: true, type: Object },
+		},
+	};
+</script>
+<style lang="scss" scoped>
+	.partnerEmployeesWrapper {
+		// background-color: white;
+		height: 100%;
+
+		.country-title {
+			margin-left: 10px;
+			margin-top: 14px;
+			font-size: 20px;
+			font-weight: 600;
+			color: $tertiary;
+		}
+		.employee-search-bar {
+			padding-top: 14px;
+			width: 100%;
+		}
+	}
+</style>
---
+++ src/views/manage-agents/index.vue
@@ -0,0 +1,430 @@
+<template>
+	<div class="partnersWrapper primary-background-color">
+		<!-- <Users v-bind="{ ...ele.props }"></Users> -->
+		<v-row class="px-6 managepartners-search-bar" justify="center" align="center">
+			<v-col cols="12" sm="8" md="6">
+				<!-- @queryString="queryString" -->
+				<Search
+					@queryString="queryString"
+					@filterObject="advanceSearch"
+					@clearFilter="advanceSearch"
+					:placeholder="placeholder"
+					:isAdvanceSearch="true"
+					:filterConfig="selectedSearchConfig"
+				></Search>
+			</v-col>
+		</v-row>
+
+		<div class="card-wrapper">
+			<div v-for="company in companyList" :key="company._id" class="card-element">
+				<InformationCard :expandCard="true">
+					<template v-slot:topLeft>
+						{{ company.business_types.join(", ") }}
+					</template>
+					<template v-slot:mainContent>
+						{{ company.name }}
+					</template>
+					<template v-slot:mainContentSubtitle>
+						{{ company.website }}
+					</template>
+					<template v-slot:mainContentRight>
+						<div class="grade">
+							{{ company.admin_grade }}
+						</div>
+					</template>
+					<template v-slot:actionButtons>
+						<template>
+							<v-btn
+								v-if="userType == ADMIN || userType == MANAGEMENT"
+								@click="disableCompany(company)"
+								color="error"
+								text
+							>
+								{{ company.record.active ? "Disable" : "Enable" }}
+							</v-btn>
+							<v-btn
+								v-if="userType == ADMIN || userType == MANAGEMENT"
+								@click="openInputForm(true, company)"
+								color="secondary"
+								text
+							>
+								Edit
+							</v-btn>
+							<v-btn @click="openEmployeeModal(company)" color="primary" text>
+								View
+							</v-btn>
+						</template>
+					</template>
+					<template v-slot:expandCardContent>
+						<v-list>
+							<v-list-item
+								v-for="(grade, index) in company.grading"
+								:key="company._id + '+' + index"
+								two-line
+							>
+								<v-list-item-icon>
+									<v-icon color="secondary">
+										grade.grade
+									</v-icon>
+								</v-list-item-icon>
+
+								<v-list-item-content>
+									<v-list-item-title>{{ grade.product }}</v-list-item-title>
+								</v-list-item-content>
+								<!-- <v-divider inset></v-divider> -->
+							</v-list-item>
+						</v-list>
+					</template>
+				</InformationCard>
+			</div>
+		</div>
+
+		<div class="text-center">
+			<v-pagination
+				@input="updatedPageNo"
+				v-model="pageNo"
+				:length="Math.ceil(fetchCount / pageSize)"
+			></v-pagination>
+		</div>
+
+		<ViewMoreModal @closeModal="viewMoreModal = false" :toggleModal="viewMoreModal">
+			<template v-slot:modalTitle>
+				<div v-if="selectedCompanyInfo.name">
+					{{ selectedCompanyInfo.name }}
+				</div>
+			</template>
+			<template v-slot:modalSubtitle>
+				<div class="tertiary-font-color" v-if="selectedCompanyInfo.business_types">
+					{{ selectedCompanyInfo.business_types.join("/ ") }}
+				</div>
+			</template>
+			<template v-slot:modalContent>
+				<companyInfo :partnerInfo="selectedCompanyInfo"></companyInfo>
+			</template>
+		</ViewMoreModal>
+
+		<UserForm
+			@formOutput="formOutput"
+			@closeForm="closeForm"
+			:name="name"
+			:inputConfig="inputConfig"
+			:keysToWatch="keysToWatch"
+			:toggleForm="toggleForm"
+			:formData="rowToEdit"
+			:isEditMode="isEditMode"
+		></UserForm>
+
+		<div class="floating-button">
+			<v-btn @click="openInputForm()" color="primary" dark fab>
+				<v-icon>mdi-plus</v-icon>
+			</v-btn>
+		</div>
+	</div>
+</template>
+
+<script>
+	import defaultCRUDMixin from "../../mixins/defaultCRUDMixins";
+	import { required, email, minLength, numeric, alpha } from "vuelidate/lib/validators";
+	import { mapActions, mapGetters, mapMutations } from "vuex";
+	import helpers from "../../components/helpers";
+	import companyInfo from "./CompanyInfo";
+	import ViewMoreModal from "../../components/ViewMoreModal";
+
+	export default {
+		name: "ManageAgents",
+		mixins: [defaultCRUDMixin],
+		components: {
+			companyInfo,
+		},
+		created() {
+			this.getCompanies();
+			this.setSearchConfig();
+		},
+		data: () => ({
+			companyList: [
+				{
+					_id: "5f857918e43f60826225",
+					name: "Thomas Cook",
+					business_types: [FIT, GIT, MICE, ADHOC, LUXURY],
+					website: "thomascook.in",
+					grading: [
+						{
+							product: "United State",
+							grade: "A",
+						},
+					],
+					admin_grade: "A",
+					blacklist: "false",
+					record: {
+						active: true,
+						created_on: "date",
+						updated_on: "date",
+					},
+				},
+				{
+					_id: "5f857918e43f76087225",
+					name: "SOTC",
+					business_types: [FIT, LUXURY],
+					website: "sotc.com",
+					grading: [
+						{
+							product: "Vietnam",
+							grade: "C",
+						},
+						{
+							product: "Afghanistan",
+							grade: "C",
+						},
+					],
+					admin_grade: "B",
+					blacklist: "false",
+					record: {
+						active: true,
+						created_on: "date",
+						updated_on: "date",
+					},
+				},
+			],
+			search_text: "",
+			name: "Travel Agents",
+			keysToWatch: ["product"],
+			inputConfig: [
+				{
+					name: "Company Name*",
+					type: "String",
+					key: "name",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+					},
+				},
+				{
+					name: "Website Info*",
+					type: "String",
+					key: "website",
+					width: "half",
+				},
+				{
+					name: "Business Type*",
+					type: "Dropdown",
+					key: "business_types",
+					width: "half",
+					multi: true,
+					isListInStore: true,
+					listVariable: "businessType",
+					validations: {
+						required,
+					},
+				},
+				{
+					name: "Admin Grade",
+					type: "String",
+					key: "name",
+					width: "half",
+				},
+				{
+					name: "Countries*",
+					type: "Dropdown",
+					key: "product",
+					width: "full",
+					multi: true,
+					isListInStore: true,
+					listVariable: "countries",
+					validations: {
+						required,
+					},
+				},
+				{
+					name: "Grade for the selected country",
+					type: "MultiInputWithGroupKey",
+					key: "grade",
+					width: "half",
+					keyToGroup: "product",
+				},
+			],
+			selectedCompanyInfo: {},
+			placeholder: "Search Companies",
+		}),
+		computed: {},
+		methods: {
+			...mapActions("ManageAgents", ["getCompaniesList", "addCompany", "editCompany"]),
+			getCompanies() {
+				this.openLoaderDialog();
+				this.getCompaniesList({
+					filter: this.filter,
+					pageSize: this.pageSize,
+					pageNo: this.pageNo,
+				}).then((data) => {
+					this.closeLoaderDialog();
+					this.companyList = data.list;
+					this.totalCount = data.totalCount;
+					this.fetchCount = data.fetchCount;
+				});
+			},
+			queryString(data) {
+				this.filter["search_text"] = data;
+				this.getCompanies();
+			},
+			advanceSearch(filterObject) {
+				this.filter = { ...filterObject };
+				this.pageNo = 1;
+				this.getCompanies();
+			},
+			async formOutput(data) {
+				var tempFile = data.logo;
+				var formData = JSON.parse(JSON.stringify(data));
+				// formData.email_ids = formData.email_ids.map((data) => data.input).filter((e) => e != "");
+				formData.logo = tempFile;
+				var tempArray = [];
+				var tempObj = {};
+
+				// loop over the emergency contacts objects to convert it into theh backend format
+
+				// for (let contact of formData.emergency_contacts) {
+				// 	tempObj = {};
+				// 	for (let num of contact.input) {
+				// 		if (num.input != "") {
+				// 			tempObj["country"] = contact.groupKey;
+				// 			if (!tempObj["contacts"]) tempObj["contacts"] = [];
+				// 			tempObj["contacts"].push(num.input);
+				// 		}
+				// 	}
+				// 	if (Object.keys(tempObj).length) {
+				// 		tempArray.push(tempObj);
+				// 	}
+				// }
+				// formData.emergency_contacts = tempArray;
+
+				console.log("Before API call FormData Object", formData);
+
+				this.openLoaderDialog();
+				if (!this.isEditMode) {
+					this.addCompany(formData).then((data) => {
+						this.closeLoaderDialog();
+						if (data.ok) {
+							this.openSnackbar({ text: "Sucessfully Added Company" });
+							console.log("Add Company success");
+							this.getCompanies();
+							this.closeForm();
+						} else {
+							this.openSnackbar({ text: data.message });
+							console.log("Add Company failed");
+						}
+					});
+				} else {
+					this.editCompany(formData).then((data) => {
+						this.closeLoaderDialog();
+						if (data.ok) {
+							this.openSnackbar({ text: "Sucessfully Edited Company" });
+							console.log("Edit Company success");
+							this.getCompanies();
+							this.closeForm();
+						} else {
+							this.openSnackbar({ text: data.message });
+							console.log("Edit Company failed");
+						}
+					});
+				}
+			},
+			getEditRowObject(data) {
+				return {
+					...data,
+					_id: data._id,
+					updated_on: data.record.updated_on,
+				};
+			},
+			disableCompany(data) {
+				if (
+					window.confirm(
+						"Do you really want to " + (data.record.active ? "Disable the Company?" : "Enable the Company?")
+					)
+				) {
+					this.openLoaderDialog();
+					this.editCompany({
+						_id: data._id,
+						active: !data.record.active,
+						updated_on: data.record.updated_on,
+					}).then((data) => {
+						this.closeLoaderDialog();
+						if (data.ok) {
+							this.openSnackbar({ text: "Sucessfully Updated Company Status" });
+							console.log("Updated Company status");
+							this.getCompanies();
+							this.closeForm();
+						} else {
+							this.openSnackbar({ text: data.message });
+							console.log("Failed to Update Company status");
+						}
+					});
+				}
+			},
+			openEmployeeModal(userData) {
+				this.selectedCompanyInfo = { ...userData };
+				// console.log(this.selectedCompanyInfo);
+				this.viewMoreModal = true;
+			},
+			setSearchConfig() {
+				/*
+				 * Name of Partner - Text field - string or number - can this be empty?
+				 * Business Type - Dropdown multi Autocomplete - need some default filter provision. - can be empty in this case but not in specific cases
+				 * Countries - Dropdown multi Autocomplete - need some default filter provision. - can be empty in this case but not in specific cases
+				 */
+				this.selectedSearchConfig = [
+					{
+						name: "Partner Name",
+						key: "name",
+						type: "text",
+						inputType: "textfield",
+						defaultValue: "",
+					},
+					{
+						name: "Business Type",
+						key: "business_types",
+						multi: true,
+						inputType: "dropdown",
+						defaultValue: [],
+						isListInStore: true,
+						listVariable: "businessType",
+					},
+					{
+						name: "Countries",
+						key: "countries",
+						multi: true,
+						inputType: "dropdown",
+						defaultValue: [],
+						isListInStore: true,
+						listVariable: "countries",
+					},
+				];
+			},
+			updatedPageNo(page) {
+				// console.log("Page", page);
+				// console.log("Page Number", this.pageNo);
+				this.getCompanies();
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scopped>
+	.partnersWrapper {
+		padding: 20px 5px;
+		height: 100%;
+		// display: flex;
+		// flex-direction: column;
+	}
+	.card-image img {
+		max-width: 100%;
+	}
+	.managepartners-search-bar {
+		margin-top: 12px;
+	}
+</style>
+
+<style lang="scss">
+	.partnersWrapper .v-list-item__title,
+	.v-list-item__subtitle {
+		white-space: normal;
+	}
+</style>
