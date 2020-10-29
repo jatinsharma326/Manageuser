be4f1f3b5774c311893d8f895884c648893b4b2b
huzefakm <huzzwork@gmail.com>
2020-10-29 19:26:22 +0530

    Created vue files for address,employees and Famtrip

---
+++ src/views/manage-agents/CompanyAddresses.vue
@@ -0,0 +1,366 @@
+<template>
+	<div class="companyAddressWrapper primary-background-color">
+		<!-- <Users v-bind="{ ...ele.props }"></Users> -->
+		<v-row class="px-6 companyaddress-search-bar" justify="center" align="center">
+			<v-col cols="12" sm="8" md="6">
+				<!-- @queryString="queryString" -->
+				<Search
+					@queryString="queryString"
+					@filterObject="advanceSearch"
+					@clearFilter="advanceSearch"
+					:placeholder="placeholder"
+					:isAdvanceSearch="true"
+					:filterConfig="selectedSearchConfig"
+				></Search>
+			</v-col>
+		</v-row>
+
+		<div class="card-wrapper">
+			<div v-for="address in addressList" :key="address._id" class="card-element">
+				<InformationCard :expandCard="true" :isCardDisabled="!address.record.active">
+					<template v-slot:topLeft>
+						{{ address.branch_name }}
+					</template>
+					<template v-slot:topRight>
+						{{ address.zone }}
+					</template>
+					<template v-slot:moreInfo>
+						{{ address.address }}
+						{{ address.state }}
+						{{ address.city }}
+						{{ address.pincode }}
+					</template>
+					<template v-slot:actionButtons>
+						<template>
+							<v-btn
+								v-if="userType == ADMIN || userType == MANAGEMENT"
+								@click="disablePartner(user)"
+								color="error"
+								text
+							>
+								{{ user.record.active ? "Disable" : "Enable" }}
+							</v-btn>
+							<v-btn
+								v-if="(userType == ADMIN || userType == MANAGEMENT) && user.record.active"
+								@click="openInputForm(true, user)"
+								color="secondary"
+								text
+							>
+								Edit
+							</v-btn>
+						</template>
+					</template>
+				</InformationCard>
+			</div>
+		</div>
+
+		<div class="text-center">
+			<v-pagination
+				@input="updatedPageNo"
+				v-model="pageNo"
+				:length="Math.ceil(fetchCount / pageSize)"
+			></v-pagination>
+		</div>
+
+		<UserForm
+			@formOutput="formOutput"
+			@closeForm="closeForm"
+			:name="name"
+			:inputConfig="inputConfig"
+			:keysToWatch="keysToWatch"
+			:toggleForm="toggleForm"
+			:formData="rowToEdit"
+			:isEditMode="isEditMode"
+		></UserForm>
+
+		<div v-if="userType == ADMIN || userType == MANAGEMENT" class="floating-button">
+			<v-btn @click="openInputForm()" color="primary" dark fab>
+				<v-icon>mdi-plus</v-icon>
+			</v-btn>
+		</div>
+	</div>
+</template>
+
+<script>
+	import defaultCRUDMixin from "../../mixins/defaultCRUDMixins";
+	import { required, email, minLength, numeric, alpha } from "vuelidate/lib/validators";
+	import { mapActions, mapGetters, mapMutations } from "vuex";
+	import helpers from "../../components/helpers";
+
+	import ViewMoreModal from "../../components/ViewMoreModal";
+
+	export default {
+		name: "Partners",
+		mixins: [defaultCRUDMixin],
+		components: {},
+		created() {
+			// this.getPartners();
+			// this.setSearchConfig();
+		},
+		data: () => ({
+			addressList: [
+				{
+					_id: "5f9a990d4667e23dac5fc70d",
+					record: {
+						created_on: "2020-10-29T10:27:25.734Z",
+						updated_on: "2020-10-29T10:27:25.734Z",
+						active: true,
+					},
+					zone: "EAST",
+					branch_name: "Dadar",
+					address: "Dadar Mumbai",
+					city: "Mumbai",
+					state: "Maharashtra",
+					pincode: "400008",
+					company_id: "5f9985ac96e9d514f0e4df55",
+				},
+			],
+			search_text: "",
+			name: "Branch Address",
+			inputConfig: [
+				{
+					name: "Branch Name*",
+					type: "String",
+					key: "branch_name",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+					},
+				},
+				{
+					name: "Address*",
+					type: "String",
+					key: "address",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+					},
+				},
+				{
+					name: "State*",
+					type: "String",
+					key: "state",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+					},
+				},
+				{
+					name: "City*",
+					type: "String",
+					key: "city",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+					},
+				},
+				{
+					name: "Pincode*",
+					type: "String",
+					key: "pincode",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+					},
+				},
+				{
+					name: "Zone*",
+					type: "String",
+					key: "zone",
+					width: "half",
+					validations: {
+						required,
+						minLength: minLength(1),
+					},
+				},
+			],
+			placeholder: "Search Addresses",
+		}),
+		computed: {},
+		methods: {
+			...mapActions("PartnerManagement", ["getaddressList", "addPartner", "editPartner"]),
+			getPartners() {
+				this.openLoaderDialog();
+				this.getaddressList({
+					filter: this.filter,
+					pageSize: this.pageSize,
+					pageNo: this.pageNo,
+				}).then((data) => {
+					this.closeLoaderDialog();
+					this.addressList = data.list;
+					this.totalCount = data.totalCount;
+					this.fetchCount = data.fetchCount;
+				});
+			},
+			queryString(data) {
+				this.filter["search_text"] = data;
+				this.getPartners();
+			},
+			advanceSearch(filterObject) {
+				this.filter = { ...filterObject };
+				this.pageNo = 1;
+				this.getPartners();
+			},
+			async formOutput(data) {
+				var tempFile = data.logo;
+				var formData = JSON.parse(JSON.stringify(data));
+				formData.email_ids = formData.email_ids.map((data) => data.input).filter((e) => e != "");
+				formData.logo = tempFile;
+				var tempArray = [];
+				var tempObj = {};
+
+				// loop over the emergency contacts objects to convert it into theh backend format
+				for (let contact of formData.emergency_contacts) {
+					tempObj = {};
+					for (let num of contact.input) {
+						if (num.input != "") {
+							tempObj["country"] = contact.groupKey;
+							if (!tempObj["contacts"]) tempObj["contacts"] = [];
+							tempObj["contacts"].push(num.input);
+						}
+					}
+					if (Object.keys(tempObj).length) {
+						tempArray.push(tempObj);
+					}
+				}
+				formData.emergency_contacts = tempArray;
+
+				// remove logo key if it's empty
+				if (formData.logo) {
+					formData.logo = await helpers.toBase64(formData.logo);
+				} else {
+					delete formData.logo;
+				}
+
+				console.log("Before API call FormData Object", formData);
+
+				this.openLoaderDialog();
+				if (!this.isEditMode) {
+					this.addPartner(formData).then((data) => {
+						this.closeLoaderDialog();
+						if (data.ok) {
+							this.openSnackbar({ text: "Sucessfully Added Partner" });
+							console.log("Add Partner success");
+							this.getPartners();
+							this.closeForm();
+						} else {
+							this.openSnackbar({ text: data.message });
+							console.log("Add Partner failed");
+						}
+					});
+				} else {
+					this.editPartner(formData).then((data) => {
+						this.closeLoaderDialog();
+						if (data.ok) {
+							this.openSnackbar({ text: "Sucessfully Edited Partner" });
+							console.log("Edit Partner success");
+							this.getPartners();
+							this.closeForm();
+						} else {
+							this.openSnackbar({ text: data.message });
+							console.log("Edit Partner failed");
+						}
+					});
+				}
+			},
+			getEditRowObject(data) {
+				return {
+					...data,
+					_id: data._id,
+					updated_on: data.record.updated_on,
+				};
+			},
+			disablePartner(data) {
+				if (
+					window.confirm(
+						"Do you really want to " + (data.record.active ? "Disable the Partner?" : "Enable the Partner?")
+					)
+				) {
+					this.openLoaderDialog();
+					this.editPartner({
+						_id: data._id,
+						active: !data.record.active,
+						updated_on: data.record.updated_on,
+					}).then((data) => {
+						this.closeLoaderDialog();
+						if (data.ok) {
+							this.openSnackbar({ text: "Sucessfully Updated Partner Status" });
+							console.log("Updated Partner status");
+							this.getPartners();
+							this.closeForm();
+						} else {
+							this.openSnackbar({ text: data.message });
+							console.log("Failed to Update Partner status");
+						}
+					});
+				}
+			},
+			setSearchConfig() {
+				/*
+				 * Name of Partner - Text field - string or number - can this be empty?
+				 * Business Type - Dropdown multi Autocomplete - need some default filter provision. - can be empty in this case but not in specific cases
+				 * Countries - Dropdown multi Autocomplete - need some default filter provision. - can be empty in this case but not in specific cases
+				 */
+				this.selectedSearchConfig = [
+					{
+						name: "Partner Name",
+						key: "name",
+						type: "text",
+						inputType: "textfield",
+						defaultValue: "",
+					},
+					{
+						name: "Business Type",
+						key: "business_types",
+						multi: true,
+						inputType: "dropdown",
+						defaultValue: [],
+						isListInStore: true,
+						listVariable: "businessType",
+					},
+					{
+						name: "Countries",
+						key: "countries",
+						multi: true,
+						inputType: "dropdown",
+						defaultValue: [],
+						isListInStore: true,
+						listVariable: "countries",
+					},
+				];
+			},
+			updatedPageNo(page) {
+				// console.log("Page", page);
+				// console.log("Page Number", this.pageNo);
+				this.getPartners();
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scopped>
+	.companyAddressWrapper {
+		padding: 20px 5px;
+		height: 100%;
+		// display: flex;
+		// flex-direction: column;
+	}
+	.card-image img {
+		max-width: 100%;
+	}
+	.companyaddress-search-bar {
+		margin-top: 12px;
+	}
+</style>
+
+<style lang="scss">
+	.companyAddressWrapper .v-list-item__title,
+	.v-list-item__subtitle {
+		white-space: normal;
+	}
+</style>
