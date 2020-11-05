4ab08b125e9774a65a29f36b5cc8632338844d45
huzefakm <huzzwork@gmail.com>
2020-11-05 18:52:40 +0530

    add: getUploadLogs, uploadTravelAgents and downloadsamples API calls

--- src/views/manage-agents/store.js
+++ src/views/manage-agents/store.js
@@ -82,6 +82,105 @@ export default {
 					return { ok: false, totalCount: 0, fetchCount: 0, list: [] };
 				});
 		},
+		// change URL for the below API call
+		getUploadLogs: ({ commit, dispatch }, payload) => {
+			let fail = (msg) => commit("failure", msg);
+			return dispatch(
+				"apiCall",
+				{
+					method: "get",
+					params: payload,
+					// url: constants.ALL_STATES,
+				},
+				{ root: true }
+			)
+				.then((data) => {
+					if (data.ok) {
+						return {
+							ok: true,
+							list: data.data,
+							fetchCount: data.fetchCount,
+							totalCount: data.totalCount,
+						};
+					} else {
+						fail(data.message || "Failed to load upload Logs List");
+						return {
+							ok: false,
+							list: [],
+						};
+					}
+				})
+				.catch((err) => {
+					console.log("Yo ", err);
+					fail(err.toString() || "Failed to load upload Logs List");
+					return { ok: false, totalCount: 0, fetchCount: 0, list: [] };
+				});
+		},
+		// change URL for the below API call
+		uploadTravelAgents: ({ commit, dispatch }, payload) => {
+			let fail = (msg) => commit("failure", msg);
+			return dispatch(
+				"apiCallWithHeaderConfig",
+				{
+					partConfig: {
+						method: "post",
+						data: payload,
+						// url: constants.GLOBAL_SETTINGS_POLICIES,
+					},
+					headerConfig: {
+						"Content-Type": "multipart/form-data",
+					},
+				},
+				{ root: true }
+			)
+				.then((data) => {
+					if (!data.ok) fail(data.message || "Failed to upload travel agent file");
+					return data;
+				})
+				.catch((err) => {
+					fail(err.toString() || "Failed to upload travel agent file");
+					return {
+						ok: false,
+						message: err.message,
+					};
+				});
+		},
+		// change URL for the below API call
+		downloadSample: ({ commit, dispatch }) => {
+			let fail = (msg) => commit("failure", msg);
+			return dispatch(
+				"fileDownload_API_Call",
+				{
+					method: "get",
+					params: {},
+					// url: constants.GLOBAL_SETTINGS_POLICIES,
+					responseType: "blob",
+				},
+				{ root: true }
+			)
+				.then(({ data, response }) => {
+					if (response.status === 200) {
+						commit("openSnackbar", { text: "Starting Download" }, { root: true });
+						console.log("Test Console for Download Policies Data", data);
+						const url = window.URL.createObjectURL(new Blob([data]));
+						const link = document.createElement("a");
+						link.href = url;
+						link.setAttribute("download", "sample.xlsx"); //or any other extension
+						document.body.appendChild(link);
+						link.click();
+						return;
+					} else {
+						commit("openSnackbar", { text: "Could not start download" }, { root: true });
+						fail(data.message || "Failed to start download");
+						return;
+					}
+				})
+				.catch((err) => {
+					console.log("Yo ", err);
+					commit("openSnackbar", { text: "Could not start download" }, { root: true });
+					fail(err.toString() || "Failed to Download Core Data File");
+				});
+		},
 
 		getCompaniesList: ({ commit, dispatch }, payload) => {
 			let fail = (msg) => commit("failure", msg);
