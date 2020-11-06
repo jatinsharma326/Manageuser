8de23689fb9ed52d89c48590cfb5ea907547c7b6
huzefakm <huzzwork@gmail.com>
2020-11-06 09:45:15 +0530

    add: computed function to check usertype This is to show / hide components accordingly

--- src/mixins/defaultCRUDMixins.js
+++ src/mixins/defaultCRUDMixins.js
@@ -12,6 +12,20 @@ const defaultCRUDMixins = {
 	}),
 	computed: {
 		...mapGetters(["REMOTE_SALES_AGENT", "SALES_AGENT", "MANAGEMENT", "ADMIN", "userType"]),
+		isAdminOrManagement: function() {
+			if (userType == ADMIN || userType == MANAGEMENT) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+		isSalesTeamMember: function() {
+			if (userType == SALES_AGENT || userType == REMOTE_SALES_AGENT) {
+				return true;
+			} else {
+				return false;
+			}
+		},
 	},
 	methods: {
 		...mapMutations(["openLoaderDialog", "closeLoaderDialog", "openSnackbar"]),
