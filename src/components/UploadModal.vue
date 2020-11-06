9362cb48db167dd040060d604e815929633a0ad6
huzefakm <huzzwork@gmail.com>
2020-11-06 18:32:56 +0530

    add: upload functionality

--- src/components/UploadModal.vue
+++ src/components/UploadModal.vue
@@ -1,12 +1,13 @@
 <template>
-	<div class="uploadWrapper">
+	<div class="uploadModalWrapper">
 		<v-row justify="center">
 			<v-dialog v-model="modal" width="600px" persistent>
-				<v-card>
+				<v-card class="upload-card">
 					<v-card-title>
-						<span class="headline">Bulk Upload Travel Agent</span>
+						<span class="headline">{{ title }}</span>
+						<v-spacer></v-spacer>
 						<div class="close-dialog" @click="closeModal">
-							<v-icon>mdi-close</v-icon>
+							<v-icon color="error">mdi-close</v-icon>
 						</div>
 					</v-card-title>
 					<div class="file-upload-section">
@@ -14,7 +15,7 @@
 							v-model="file"
 							type="file"
 							class="upload-file-selector"
-							accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+							:accept="accept"
 						></v-file-input>
 						<div class="upload-actions">
 							<v-btn
@@ -26,12 +27,12 @@
 								>Upload</v-btn
 							>
 							<v-btn
+								v-if="showDownloadSampleButton"
 								color="green"
 								class="action-button"
-								v-if="sampleFile"
 								@click="downloadSampleFile()"
 								text
-								>Download</v-btn
+								>Download Sample</v-btn
 							>
 						</div>
 					</div>
@@ -58,44 +59,32 @@
 	</div>
 </template>
 <script>
-	import helperMixin from "../mixins/helperMixins";
 	import { mapActions, mapGetters, mapMutations } from "vuex";
 	export default {
 		name: "UploadModal",
-		mixins: [helperMixin],
+		mixins: [],
 		components: {},
 		created() {},
 		data: () => ({
 			modal: false,
-			logsList: [],
-			filter: {},
-			fetchCount: 0,
-			totalCount: 0,
-			pageSize: 20,
-			pageNo: 1,
-			sampleFile: false,
+			file: false,
 			showProgress: false,
 			file: null,
 		}),
 		methods: {
 			...mapMutations(["openLoaderDialog", "closeLoaderDialog", "openSnackbar"]),
-			...mapActions("ManageAgents", ["uploadTravelAgents", "downloadSample"]),
+			...mapActions("ManageAgents", ["downloadSample"]),
 			closeModal() {
-				this.pageSize = 20;
-				this.logsList = [];
 				this.$emit("closeModal");
 			},
-			loadMoreLogs() {
-				this.pageSize = this.pageSize + 20;
-				this.getAllUploadLogs(this.selectedInfo);
-			},
 			uploadFile() {
-				this.showProgress = true;
 				let formData = this.createFormData(this.file);
-				this.uploadTravelAgents(formData).then((data) => {
+				this.showProgress = true;
+				this.uploadFunction(formData).then((data) => {
 					this.showProgress = false;
 					if (data.ok) {
 						this.openSnackbar({ text: "File Upload Sucessful" });
+						this.closeModal();
 						this.file = null;
 					} else {
 						this.openSnackbar({ text: data.message });
@@ -104,11 +93,12 @@
 			},
 			createFormData(file) {
 				let formData = new FormData();
-				formData.append("sampleFile", file);
+				formData.append("travelAgents", file);
+				if (this.process_id) formData.append("process_id", this.process_id);
 				return formData;
 			},
 			downloadSampleFile() {
-				this.downloadSample().then(() => {});
+				this.downloadSampleFunc().then(() => {});
 			},
 		},
 		computed: {
@@ -117,14 +107,26 @@
 		watch: {
 			toggleModal(nv, ov) {
 				this.modal = nv;
-				this.logsList = [];
-				// if (nv) this.getAllUploadLogs(this.selectedInfo);
+				if (nv) this.file = null;
 			},
 		},
 		props: {
 			toggleModal: { required: true, default: false },
-			// selectedInfo: { required: true, type: Object },
+			accept: { required: true, type: String },
+			title: { required: true, type: String },
+			uploadFunction: { required: true, type: Function },
+			downloadSampleFunc: { required: false, type: Function },
+			showDownloadSampleButton: { required: true, type: Boolean },
+			process_id: { required: false, type: String },
 		},
 	};
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	.uploadModalWrapper {
+		.file-upload-section {
+			padding: 20px;
+			display: flex;
+			justify-content: center;
+		}
+	}
+</style>
