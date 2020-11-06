a27db463471d4ba310902e4b687b2c54437a2c00
huzefakm <huzzwork@gmail.com>
2020-11-06 15:44:59 +0530

    fix: z-index value to 300

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@
 			</v-btn>
 		</v-snackbar>
 
-		<v-overlay class="app-loader" :value="loaderDialog" :z-index="203">
+		<v-overlay class="app-loader" :value="loaderDialog" :z-index="300">
 			<v-progress-circular color="primary" indeterminate size="64"></v-progress-circular>
 		</v-overlay>
 	</v-app>
