8d0b8b331cfb2ec6975588d74641d98729c33ec6
huzefakm <huzzwork@gmail.com>
2020-11-08 10:03:50 +0530

    Release v3.0.0 End of Travel Agent Section

--- src/Auth.vue
+++ src/Auth.vue
@@ -2,7 +2,7 @@
 	<div class="authWrapper">
 		<div class="auth-page-title">
 			Global Destinations CRM <br />
-			V 2.0.1
+			V 3.0.0
 		</div>
 		<div class="auth-input-wrapper">
 			<div class="form-title">Login</div>
