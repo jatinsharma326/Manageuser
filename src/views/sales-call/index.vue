1cb8ffb55850401e893ebdb658c2076d1c79ffd5
huzefakm <huzzwork@gmail.com>
2020-11-10 17:03:48 +0530

    add: min max functionality for date in form config

--- src/views/sales-call/index.vue
+++ src/views/sales-call/index.vue
@@ -12,20 +12,36 @@
 </template>
 
 <script>
+	import defaultCRUDMixin from "../../mixins/defaultCRUDMixins";
 	import { required, email, minLength, numeric, alpha } from "vuelidate/lib/validators";
 	import { mapActions, mapGetters } from "vuex";
 	import callsList from "./callsList";
+	import moment from "moment-timezone";
 	export default {
 		name: "ManageSalesCall",
+		mixins: [defaultCRUDMixin],
 		components: { callsList },
 		async created() {
-			await this.getCompanies();
-			this.setConfig(this.companyList);
+			if (this.isSalesTeamMember) {
+				await this.getCompanies();
+				this.setConfig(this.companyList);
+			}
 		},
 		data: () => ({
 			tab: "",
 			companyList: [],
-			tabConfig: [],
+			tabConfig: [
+				{
+					name: "All Sales Call",
+					id: "allSalesCall",
+					component: "Users",
+					props: {
+						name: "All Sales Call",
+						type: "all_sales_call",
+						placeholder: "Search All Sales Call",
+					},
+				},
+			],
 		}),
 		computed: {
 			...mapGetters([]),
@@ -40,112 +56,114 @@
 				});
 			},
 			setConfig(companyList = []) {
-				this.tabConfig = [
-					{
+				this.tabConfig.unshift({
+					name: "Sales Call",
+					id: "salesCall",
+					component: "Users",
+					props: {
 						name: "Sales Call",
-						id: "salesCall",
-						component: "Users",
-						props: {
-							name: "Sales Call",
-							type: "sales_call",
-							placeholder: "Search Sales Call",
-							inputConfig: [
-								{
-									name: "Date of Visit*",
-									type: "Date",
-									key: "date_of_call",
-									width: "half",
-									validations: {
-										required,
-									},
+						type: "sales_call",
+						placeholder: "Search Sales Call",
+						inputConfig: [
+							{
+								name: "Date of Visit*",
+								type: "Date",
+								key: "date_of_call",
+								min: () => {
+									return moment()
+										.tz("Asia/Kolkata")
+										.subtract(1, "month")
+										.startOf("month")
+										.format("YYYY-MM-DD");
 								},
-								// {
-								// 	name: "Company Name*",
-								// 	type: "Dropdown",
-								// 	key: "company_id",
-								// 	width: "half",
-								// 	multi: false,
-								// 	isListInStore: false,
-								// 	listItems: companyList,
-								// 	validations: {
-								// 		required,
-								// 	},
-								// },
-								{
-									name: "Company Name*",
-									type: "DropdownWithMoreInfo",
-									isCustom: true,
-									subtitleContent: (item) => {
-										return item.business_types.join(", ");
-									},
-									titleContent: (item) => {
-										return item.name;
-									},
-									apiCall: (company_id) => {
-										// return function getAddresses() {
-										return this.getAddressList({
-											filter: {
-												company_id: company_id,
-											},
-										}).then((data) => {
-											return {
-												data,
-											};
-										});
-										// };
-									},
-									key: "company_id",
-									width: "half",
-									multi: false,
-									isListInStore: false,
-									listItems: companyList,
-									itemText: "name",
-									itemValue: "_id",
-									validations: {
-										required,
-									},
+								max: () => {
+									return moment()
+										.tz("Asia/Kolkata")
+										.add(2, "month")
+										.endOf("month")
+										.format("YYYY-MM-DD");
 								},
-								{
-									name: "Branch Name*",
-									type: "AsyncDropdownWithMoreInfo",
-									triggerKey: "company_id",
-									subtitleContent: (item) => {
-										return item.address + " " + item.state + " " + item.city + " " + item.pincode;
-									},
-									titleContent: (item) => {
-										return item.branch_name;
-									},
-									apiCall: (company_id) => {
-										return this.getAddressList({
-											filter: {
-												company_id: company_id,
-											},
-										}).then((data) => {
-											return data.list;
-										});
-									},
-									key: "company_address_id",
-									width: "half",
-									itemText: "branch_name",
-									itemValue: "_id",
-									validations: {
-										required,
-									},
+								width: "half",
+								validations: {
+									required,
 								},
-							],
-						},
+							},
+							// {
+							// 	name: "Company Name*",
+							// 	type: "Dropdown",
+							// 	key: "company_id",
+							// 	width: "half",
+							// 	multi: false,
+							// 	isListInStore: false,
+							// 	listItems: companyList,
+							// 	validations: {
+							// 		required,
+							// 	},
+							// },
+							{
+								name: "Company Name*",
+								type: "DropdownWithMoreInfo",
+								isCustom: true,
+								subtitleContent: (item) => {
+									return item.business_types.join(", ");
+								},
+								titleContent: (item) => {
+									return item.name;
+								},
+								apiCall: (company_id) => {
+									// return function getAddresses() {
+									return this.getAddressList({
+										filter: {
+											company_id: company_id,
+										},
+									}).then((data) => {
+										return {
+											data,
+										};
+									});
+									// };
+								},
+								key: "company_id",
+								width: "half",
+								multi: false,
+								isListInStore: false,
+								listItems: companyList,
+								itemText: "name",
+								itemValue: "_id",
+								validations: {
+									required,
+								},
+							},
+							{
+								name: "Branch Name*",
+								type: "AsyncDropdownWithMoreInfo",
+								triggerKey: "company_id",
+								subtitleContent: (item) => {
+									return item.address + " " + item.state + " " + item.city + " " + item.pincode;
+								},
+								titleContent: (item) => {
+									return item.branch_name;
+								},
+								apiCall: (company_id) => {
+									return this.getAddressList({
+										filter: {
+											company_id: company_id,
+										},
+									}).then((data) => {
+										return data.list;
+									});
+								},
+								key: "company_address_id",
+								width: "half",
+								itemText: "branch_name",
+								itemValue: "_id",
+								validations: {
+									required,
+								},
+							},
+						],
 					},
-					{
-						name: "All Sales Call",
-						id: "allSalesCall",
-						component: "Users",
-						props: {
-							name: "All Sales Call",
-							type: "all_sales_call",
-							placeholder: "Search All Sales Call",
-						},
-					},
-				];
+				});
 			},
 		},
 	};
