6182987ea9d57dd59cd75ede2236d2306f354495
huzefakm <huzzwork@gmail.com>
2020-11-13 17:01:38 +0530

    add: search mixin and helpermixin refactor: remove action and followup

--- src/views/monthly-sales-report/ReportView.vue
+++ src/views/monthly-sales-report/ReportView.vue
@@ -18,29 +18,11 @@
 				<template v-slot:[`item.travel_agent_employee.email_ids`]="{ item }">
 					{{ item.travel_agent_employee.email_ids.join(", ") }}
 				</template>
-				<template v-slot:[`item.follow_up_on_date`]="{ item }">
-					{{ item.follow_up_on_date ? getFormattedDate(item.follow_up_on_date, "MMMM Do YYYY dddd") : "-" }}
-				</template>
 				<template v-slot:expanded-item="{ headers, item }">
 					<td class="expandable-section table-expanded-background " :colspan="headers.length">
 						<div class="expandable-section-title">Remark</div>
 						<div class="expandable-section-content" v-html="item.meeting_remark">{{}}</div>
 					</td>
-				</template>
-				<template v-slot:[`item.actions`]="{ item }">
-					<template v-if="canUserEdit(item)">
-						<v-menu bottom left>
-							<template v-slot:activator="{ on, attrs }">
-								<v-btn icon v-bind="attrs" v-on="on">
-									<v-icon>mdi-dots-vertical</v-icon>
-								</v-btn>
-							</template>
-							<v-list>
-								<v-list-item @click="openInputForm(true, item)">EDIT</v-list-item>
-								<v-list-item @click="deleteCall(item)">DELETE</v-list-item>
-							</v-list>
-						</v-menu>
-					</template>
 				</template>
 			</v-data-table>
 		</div>
@@ -58,11 +40,13 @@
 
 <script>
 	import defaultCRUDMixin from "../../mixins/defaultCRUDMixins";
+	import helperMixins from "../../mixins/helperMixins";
+	import searchMixin from "../../mixins/searchMixin";
 	import { mapActions, mapGetters, mapMutations } from "vuex";
 
 	export default {
 		name: "MSRReportView",
-		mixins: [defaultCRUDMixin],
+		mixins: [searchMixin, helperMixins, defaultCRUDMixin],
 		created() {
 			this.getData();
 		},
@@ -106,19 +90,16 @@
 				{ text: "Sr. No.", align: "start", value: "sales_call_data.sr_no", width: 100 },
 				{ text: "Name", value: "mortal_data.name", width: 150 },
 				{ text: "Date of Visit", value: "sales_call_data.date_of_call", width: 200 },
-				{ text: "Company Name", value: "company_data.name", width: 200 },
-				{ text: "Branch Name", value: "company_address_data.branch_name", width: 150 },
-				{ text: "City", value: "company_address_data.city", width: 150 },
-				{ text: "State", value: "company_address_data.state", width: 150 },
-				{ text: "Address", value: "company_address_data.address", width: 150 },
-				{ text: "Employee", value: "travel_agent_employee.name", width: 150 },
-				{ text: "Designation", value: "travel_agent_employee.designation", width: 150 },
-				{ text: "Contact No.", value: "travel_agent_employee.phone_numbers", width: 150 },
-				{ text: "Email ID", value: "travel_agent_employee.email_ids", width: 150 },
-				{ text: "Follow Up", value: "follow_up_on_date", width: 150 },
-				{ text: "Status", value: "status", width: 150 },
+				{ text: "Company Name", value: "sales_call_data.company_data.name", width: 200 },
+				{ text: "Branch Name", value: "sales_call_data.company_address_data.branch_name", width: 150 },
+				{ text: "City", value: "sales_call_data.company_address_data.city", width: 150 },
+				{ text: "State", value: "sales_call_data.company_address_data.state", width: 150 },
+				{ text: "Address", value: "sales_call_data.company_address_data.address", width: 150 },
+				{ text: "Employee", value: "travel_agent_data.name", width: 150 },
+				{ text: "Designation", value: "travel_agent_data.designation", width: 150 },
+				{ text: "Contact No.", value: "travel_agent_data.phone_numbers", width: 150 },
+				{ text: "Email ID", value: "travel_agent_data.email_ids", width: 150 },
 				// { text: "Remark", value: "data-table-expand" },
-				{ text: "", value: "actions" },
 			],
 			expanded: [],
 			filter: {},
