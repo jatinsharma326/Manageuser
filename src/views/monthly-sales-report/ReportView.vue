a0c704780e19ef88c5c46d63a5d0a1bb2d13548a
huzefakm <huzzwork@gmail.com>
2020-11-14 16:03:19 +0530

    removed: default object

--- src/views/monthly-sales-report/ReportView.vue
+++ src/views/monthly-sales-report/ReportView.vue
@@ -58,41 +58,7 @@
 			this.getData();
 		},
 		data: () => ({
-			reportsList: [
-				// {
-				// 	sales_call_id: "5faa849bbbd00900275d0322",
-				// 	travel_agent_id: "5faa6ed0bbd00900275d0319",
-				// 	status: "ON GOING",
-				// 	follow_up_on_date: "2020-11-28T18:30:00.000Z",
-				// 	meeting_remark: "Met with Akshay Sir, was a productive call. Might get business.",
-				// 	mortal_id: "5f9fa9ea077a8d00252e1278",
-				// 	sales_call_data: {
-				// 		date_of_call: "2020-07-03T18:30:00.000Z",
-				// 		sr_no: "JUL20-0",
-				// 		mortal_id: "5f9fa9ea077a8d00252e1278",
-				// 		company_id: "5faa6ed0bbd00900275d0315",
-				// 		company_address_id: "5faa6ed0bbd00900275d0317",
-				// 		company_data: {
-				// 			name: "Haridwar Apps",
-				// 		},
-				// 		company_address_data: {
-				// 			branch_name: "Dhongra",
-				// 			zone: "NORTH",
-				// 			address: "Bandra",
-				// 			city: "Mumbai",
-				// 			state: "Maharashtra",
-				// 			pincode: "400008",
-				// 			company_id: "5faa6ed0bbd00900275d0315",
-				// 		},
-				// 	},
-				// 	travel_agent_data: {
-				// 		name: "Aishwarya",
-				// 		phone_numbers: ["125546"],
-				// 		designation: "ASM",
-				// 		email_ids: ["test@test.com", "d@gd.com"],
-				// 	},
-				// },
-			],
+			reportsList: [],
 			headers: [
 				{ text: "Sr. No.", align: "start", value: "sales_call_data.sr_no", width: 100 },
 				{ text: "Name", value: "mortal_data.name", width: 150 },
