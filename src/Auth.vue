1470e9b24992b385aa85582564602ea9d9150f70
huzefakm <huzzwork@gmail.com>
2020-11-16 17:08:45 +0530

    release v3.1.0

--- src/Auth.vue
+++ src/Auth.vue
@@ -2,7 +2,7 @@
 	<div class="authWrapper">
 		<div class="auth-page-title">
 			Global Destinations CRM <br />
-			V 3.0.1
+			V 3.1.0
 		</div>
 		<div class="auth-input-wrapper">
 			<div class="form-title">Login</div>
