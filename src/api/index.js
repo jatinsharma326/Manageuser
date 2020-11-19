051d5f8281155bccd7681538b5bb81d4997513b4
huzefakm <huzzwork@gmail.com>
2020-11-19 14:54:45 +0530

    add: endpoint for DSR notification

--- src/api/index.js
+++ src/api/index.js
@@ -80,4 +80,5 @@ export default {
 	EMPLOYEE_BIRTHDAYS: BASE_URL + "Mortals/v0.1/birthday-reminder",
 	DSR_REMINDER: BASE_URL + "DSR/v0.1/follow-up-reminder",
 	FOLLOW_UP_REMINDER: BASE_URL + "DSR/v0.1/follow-up-reminder",
+	DSR_NOTIFICATIONS: BASE_URL + "DSR/v0.1/dsr-for-my-products",
 };
