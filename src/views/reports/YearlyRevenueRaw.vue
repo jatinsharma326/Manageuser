e11bc178a3c1bc5a0b9a6141435c852337ddee73
huzefakm <huzzwork@gmail.com>
2020-11-28 14:07:35 +0530

    create: Yearly Revenue Raw File

---
+++ src/views/reports/YearlyRevenueRaw.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="companyInfoWrapper"></div>
+</template>
+
+<script>
+	import { mapActions, mapGetters, mapMutations } from "vuex";
+	// import CompanyAddresses from "./CompanyAddresses";
+	// import CompanyEmployees from "./CompanyEmployees";
+	// import CompanyFamTrip from "./CompanyFamTrip";
+
+	export default {
+		name: "ReportsManager",
+		components: {
+			// CompanyAddresses,
+			// CompanyEmployees,
+			// CompanyFamTrip,
+		},
+		created() {
+			// this.getPartnerList();
+		},
+		data: () => ({
+			tab: "",
+		}),
+		computed: {
+			...mapGetters([]),
+		},
+		methods: {
+			...mapActions([]),
+		},
+		props: {
+			name: { required: true, type: String },
+			placeholder: { required: false, type: String },
+			businessType: { required: false, type: Array, default: () => [] },
+			userList: { required: false, type: Array, default: () => [] },
+			countriesList: { required: false, type: Array, default: () => [] },
+		},
+	};
+</script>
+<style lang="scss">
+	.companyInfoWrapper {
+		height: 100%;
+		.tabItemWrapper {
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+		}
+	}
+</style>
