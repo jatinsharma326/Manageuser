73d7ff7d8a5d0ec76c0acba317335913190d106c
huzefakm <huzzwork@gmail.com>
2020-11-28 17:24:02 +0530

    add: tab for raw data

--- src/views/reports/ReportsManager.vue
+++ src/views/reports/ReportsManager.vue
@@ -1,63 +1,76 @@
 <template>
-	<div class="companyInfoWrapper">
-		{{ businessType }},<br />
-		{{ userList }},<br />
-		{{ countriesList }},
-		<!-- <v-tabs grow v-model="tab">
-			<v-tab>Addresses</v-tab>
-			<v-tab>Employees</v-tab>
-			<v-tab>Fam Trip</v-tab>
-		</v-tabs>
-		<v-tabs-items class="tabItemWrapper" v-model="tab">
-			<v-tab-item>
-				<CompanyAddresses :companyInfo="companyInfo"></CompanyAddresses>
-			</v-tab-item>
-			<v-tab-item>
-				<CompanyEmployees :companyInfo="companyInfo"></CompanyEmployees>
-			</v-tab-item>
-			<v-tab-item>
-				<CompanyFamTrip :companyInfo="companyInfo"></CompanyFamTrip>
-			</v-tab-item>
-		</v-tabs-items> -->
+	<div class="reportsManagerWrapper">
+		<template v-if="id == 'yearlyRevenue'">
+			<v-tabs grow v-model="tab">
+				<v-tab>Raw Data</v-tab>
+				<!-- <v-tab>Employees</v-tab>
+				<v-tab>Fam Trip</v-tab> -->
+			</v-tabs>
+			<v-tabs-items class="tabItemWrapper" v-model="tab">
+				<v-tab-item>
+					<!-- :name="name"
+						:placeholder="placeholder"
+						:userList="userList"
+						:countriesList="countriesList" -->
+					<YearlyRevenueRaw v-bind="{ ...propsList }"></YearlyRevenueRaw>
+				</v-tab-item>
+				<!-- <v-tab-item>
+					<CompanyEmployees :companyInfo="companyInfo"></CompanyEmployees>
+				</v-tab-item>
+				<v-tab-item>
+					<CompanyFamTrip :companyInfo="companyInfo"></CompanyFamTrip>
+				</v-tab-item> -->
+			</v-tabs-items>
+		</template>
 	</div>
 </template>
 
 <script>
 	import { mapActions, mapGetters, mapMutations } from "vuex";
-	// import CompanyAddresses from "./CompanyAddresses";
+	import YearlyRevenueRaw from "./YearlyRevenueRaw";
 	// import CompanyEmployees from "./CompanyEmployees";
 	// import CompanyFamTrip from "./CompanyFamTrip";
 
 	export default {
 		name: "ReportsManager",
 		components: {
-			// CompanyAddresses,
+			YearlyRevenueRaw,
 			// CompanyEmployees,
 			// CompanyFamTrip,
 		},
 		created() {
+			this.setProps();
 			// this.getPartnerList();
 		},
 		data: () => ({
 			tab: "",
+			propsList: {},
 		}),
 		computed: {
 			...mapGetters([]),
 		},
 		methods: {
 			...mapActions([]),
+			setProps() {
+				this.propsList = {
+					name: this.name,
+					placeholder: this.placeholder,
+					userList: this.userList,
+					countriesList: this.countriesList,
+				};
+			},
 		},
 		props: {
 			name: { required: true, type: String },
+			id: { required: true, type: String },
 			placeholder: { required: false, type: String },
-			businessType: { required: false, type: Array, default: () => [] },
 			userList: { required: false, type: Array, default: () => [] },
 			countriesList: { required: false, type: Array, default: () => [] },
 		},
 	};
 </script>
 <style lang="scss">
-	.companyInfoWrapper {
+	.reportsManagerWrapper {
 		height: 100%;
 		.tabItemWrapper {
 			height: 100%;
