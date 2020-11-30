2b662de6a8dfd3ce1df66fa7b590503434b02b21
huzefakm <huzzwork@gmail.com>
2020-11-30 18:34:49 +0530

    create barchart vue file

---
+++ src/components/BarChart.vue
@@ -0,0 +1,39 @@
+<script>
+	import { Bar } from "vue-chartjs";
+	// var randomColor = require("randomcolor");
+
+	export default {
+		extends: Bar,
+		props: {
+			mainAxis: {
+				type: Array,
+			},
+			secondaryAxis: {
+				type: Array,
+			},
+			options: {
+				type: Object,
+			},
+			// chartColors: {
+			// 	type: Object,
+			// },
+		},
+		updated() {
+			// function getRandomColor() {
+			// 	var letters = "0123456789ABCDEF".split("");
+			// 	var color = "#";
+			// 	for (var i = 0; i < 6; i++) {
+			// 		color += letters[Math.floor(Math.random() * 16)];
+			// 	}
+			// 	return color;
+			// }
+			this.renderChart(
+				{
+					labels: this.mainAxis,
+					datasets: this.secondaryAxis,
+				},
+				this.options
+			);
+		},
+	};
+</script>
