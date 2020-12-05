99e48497a1ad2cdc748024768a29562a22d0c883
huzefakm <huzzwork@gmail.com>
2020-12-05 11:24:18 +0530

    release 6.0.0
    Added
    - reports section
    - virtual reach

--- src/Auth.vue
+++ src/Auth.vue
@@ -2,7 +2,7 @@
 	<div class="authWrapper">
 		<div class="auth-page-title">
 			Global Destinations CRM <br />
-			V 5.0.4
+			V 6.0.0
 		</div>
 		<div class="auth-input-wrapper">
 			<div class="form-title">Login</div>
