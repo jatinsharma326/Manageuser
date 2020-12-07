36fd01b1d03e7585fea12d529ed83aafee382605
huzefakm <huzzwork@gmail.com>
2020-12-07 12:28:04 +0530

    release v6.1.0
    add: page size change functionality
    refactor: unify page number update and normal search functionality

--- src/Auth.vue
+++ src/Auth.vue
@@ -2,7 +2,7 @@
 	<div class="authWrapper">
 		<div class="auth-page-title">
 			Global Destinations CRM <br />
-			V 6.0.1
+			V 6.1.0
 		</div>
 		<div class="auth-input-wrapper">
 			<div class="form-title">Login</div>
