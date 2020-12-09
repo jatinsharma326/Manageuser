8aafb897e345c525d3d4d0f321998bffe1b6e424
huzefakm <huzzwork@gmail.com>
2020-12-09 18:04:32 +0530

    release v6.1.2
    add their logo blue color as primary color

--- src/Auth.vue
+++ src/Auth.vue
@@ -2,7 +2,7 @@
 	<div class="authWrapper">
 		<div class="auth-page-title">
 			Global Destinations CRM <br />
-			V 6.1.1
+			V 6.1.2
 		</div>
 		<div class="auth-input-wrapper">
 			<div class="form-title">Login</div>
