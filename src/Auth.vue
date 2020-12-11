d09dbb80bf9e9b14747db024e8096f0b80a3877d
huzefakm <huzzwork@gmail.com>
2020-12-11 20:15:00 +0530

    release v6.2.1
    work on feedback given by SHabbir bhai

--- src/Auth.vue
+++ src/Auth.vue
@@ -2,7 +2,7 @@
 	<div class="authWrapper">
 		<div class="auth-page-title">
 			Global Destinations CRM <br />
-			V 6.2.0
+			V 6.2.1
 		</div>
 		<div class="auth-input-wrapper">
 			<div class="form-title">Login</div>
