8a844d874599edfefab3aa62ad46a8861d05316f
huzefakm <huzzwork@gmail.com>
2021-01-18 19:58:18 +0530

    create: piechart component

---
+++ src/components/PieChart.vue
@@ -0,0 +1,22 @@
+<script>
+	import { Pie, mixins } from "vue-chartjs";
+	import { mapGetters } from "vuex";
+
+	export default {
+		extends: Pie,
+		mixins: [mixins.reactiveProp],
+		props: {
+			chartData: {
+				type: Object,
+			},
+			options: {
+				type: Object,
+			},
+		},
+		mounted() {
+			this.renderChart(this.chartData, this.options);
+		},
+		computed: {},
+		watch: {},
+	};
+</script>
