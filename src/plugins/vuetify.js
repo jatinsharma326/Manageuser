68a1bd07065ad061be1a60a66ee9bc6d4f849104
huzefakm <huzzwork@gmail.com>
2021-02-08 17:57:33 +0530

    remove: commented code update color description

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,26 +7,15 @@ export default new Vuetify({
 	theme: {
 		themes: {
 			light: {
-				// primary: "#00887a", // Primary Green (Mainly for header and navbar)
-				primary: "#37b6c9", // Primary Green (Mainly for header and navbar)
-				primaryFontColor: "#2B4266", // font navy blue, font color on the overview screen
-				secondary: "#77a6f7", // light purple,
-				secondaryFontColor: "#112d32", // font gray, font color on the master screen for the muted text
-				tertiary: "#355280", // light navy blue, this is for the expanded state of the menu item
-				tertiaryFontColor: "#ececec", // light gray,
-				accent: "#f3d250", // yellow , this is the selected state of the link
+				primary: "#37b6c9", // Primary Blue
+				primaryFontColor: "#2B4266", // font navy blue
+				secondary: "#77a6f7", // light purple
+				secondaryFontColor: "#112d32", // dark green
+				tertiary: "#355280", // light navy blue
+				tertiaryFontColor: "#ececec", // light gray
+				accent: "#f3d250", // yellow
 				error: "#940000", // red
 			},
-			// light: {
-			// 	primary: '#2B4266', // navy blue
-			// 	primaryFontColor: '#2B4266', // font navy blue, font color on the overview screen
-			// 	secondary: '#20324D', // dark navy blue, this is the selected state of the link
-			// 	secondaryFontColor: '#aaacb2', // font gray, font color on the master screen for the muted text
-			// 	tertiary: '#355280', // light navy blue, this is for the expanded state of the menu item
-			// 	tertiaryFontColor: '#333333', // Gaurav modi black
-			// 	accent: '#3DFFE5', // neon blue
-			// 	error: '#FF5D5D', // red
-			// },
 		},
 	},
 });
