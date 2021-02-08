acff2422cb8c334697bd3d8b5a037f17bc94ddc4
huzefakm <huzzwork@gmail.com>
2021-02-08 17:23:12 +0530

    add: comment to searchMixins

--- src/mixins/searchMixin.js
+++ src/mixins/searchMixin.js
@@ -20,6 +20,7 @@ const searchMixins = {
 		search_text: "",
 	}),
 	computed: {
+		// This computed function divides the fetchcount with the pagesize to determine if pagination needs to be displayed or not.
 		isPaginationRequired: function() {
 			return !(this.fetchCount / this.pageSize <= 1);
 		},
@@ -28,19 +29,23 @@ const searchMixins = {
 		...mapMutations(["openSnackbar"]),
 		checkForErrorMessage(data, key) {
 			if (data.totalCount === 0 && data.ok) {
+				// Enters This condition when theres no data in the database, hence totalcount = 0 and data is 'ok' which means there was no errors in fetching the data
 				this.showErrorMessage = true;
 				this.errorMessage = "No data found.";
 				return null;
 			} else if (data.fetchCount === 0 && data.ok) {
+				// Enters This condition when theres is data in the database but still nothing was fetched, for example when we filter things when we use advance search, hence fetchcount = 0 and data is 'ok' which means there is no errors in fetching the data
 				this.showErrorMessage = true;
 				this.errorMessage = "No Results for your Search. Please try again.";
 				return null;
 			} else if (!data.ok) {
+				// Enters This condition data.ok is set to false which means there was an error while fetching the data
 				this.openSnackbar({ text: `Failed to Fetch ${key} Data` });
 				this.showErrorMessage = true;
 				this.errorMessage = `Failed to Fetch ${key} Data. Please Refresh`;
 				return null;
 			} else {
+				// Enters This condition when Everything is fine
 				this.showErrorMessage = false;
 				this.totalCount = data.totalCount;
 				this.fetchCount = data.fetchCount;
